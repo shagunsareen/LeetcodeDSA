6eada6b9892cd29e207156bf5d0fbac6e167a8c1
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-04-20 21:03:39 +0530

    Time: 4 ms (98.68%), Space: 45.8 MB (38.40%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -7,10 +7,17 @@ class Solution {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    if (i == 0 || grid[i - 1][j] == 0) num++; // UP
-                    if (j == 0 || grid[i][j - 1] == 0) num++; // LEFT
-                    if (i == rows -1 || grid[i + 1][j] == 0) num++; // DOWN
-                    if (j == cols -1 || grid[i][j + 1] == 0) num++; // RIGHT
+                    num += 4; //consider it as a best case with no neighbours
+                    
+                    //check if it has up neighbour 
+                    if(i > 0 && grid[i-1][j]==1){
+                        num -= 2;
+                    }
+                    
+                    //check if it has left neighbour
+                    if(j>0 && grid[i][j-1]==1){
+                        num -=2;
+                    }
                 }
             }
         }
