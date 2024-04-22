bc1fb2d67864eee05bcdfec87a94a100ec702528
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-04-23 00:46:33 +0530

    Time: 191 ms (28.55%), Space: 191.3 MB (8.59%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,5 +1,5 @@
 class Solution {    
-    private boolean seen;
+    /*private boolean seen;
     
     public boolean validPath(int n, int[][] edges, int start, int end) {
         boolean[] visited = new boolean[n];
@@ -36,5 +36,45 @@ class Solution {
                 dfs(graph, visited, neighbor, end);
             }
         }
+    }*/
+    
+     public boolean validPath(int n, int[][] edges, int start, int end) {
+         if(start == end){
+             return true;
+         }
+         
+         //make adjacency list 
+         Map<Integer, List<Integer>> adjacencyMap = new HashMap<>();
+         
+         for(int[] edge : edges){
+             int u = edge[0];
+             int v = edge[1];
+             adjacencyMap.computeIfAbsent(u, k-> new ArrayList<>()).add(v);
+             adjacencyMap.computeIfAbsent(v, k-> new ArrayList<>()).add(u);
+         }
+         
+         boolean[] visited = new boolean[n];
+         return dfs(start, end, adjacencyMap, visited);
+     }
+    
+    private boolean dfs(int start, int end, Map<Integer, List<Integer>> graph, boolean[] visited){
+        if(start == end){
+            return true;
+        }
+
+        visited[start] = true; //mark current node as visited 
+        
+        //call dfs for adjacent nodes to find the path
+        for(Integer neighbor : graph.getOrDefault(start, new ArrayList<>()))
+        {
+            if(!visited[neighbor])
+            { //check if neighbour is not present in the visited array we can iterate on it.
+                if(dfs(neighbor, end, graph, visited))
+                {
+                    return true;
+                }  
+            } 
+        }
+        return false;
     }
 }
