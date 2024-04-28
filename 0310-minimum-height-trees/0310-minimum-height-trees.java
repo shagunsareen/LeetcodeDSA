6f46f5d0146b4434e5e6e0b42dbe5e965b71ca8c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-04-28 17:31:29 +0530

    Time: 28 ms (46.92%), Space: 58.1 MB (11.82%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,25 +1,31 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n == 1) return Collections.singletonList(0);
-
-        List<Set<Integer>> adj = new ArrayList<>(n);
-        for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
-        for (int[] edge : edges) {
+        if(n==1) return Collections.singletonList(0);
+        
+        //make adjacency list
+        ArrayList<HashSet<Integer>> adj = new ArrayList<>(n);
+        for(int i=0; i<n; i++) adj.add(new HashSet<>());
+        for(int[] edge : edges){
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-
+        
         List<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < n; ++i)
-            if (adj.get(i).size() == 1) leaves.add(i);
-
-        while (n > 2) {
-            n -= leaves.size();
+        //get the leaf nodes 
+        for(int i=0; i<n; i++){
+            if(adj.get(i).size() == 1) leaves.add(i);
+        }
+        
+        //There can be either 1 root node or 2 root nodes which will have minimum height trees
+        //remove leave nodes, add new leaves nodes till we get center root nodes
+        while(n>2){
+            n -= leaves.size(); //these many nodes will be excluded from the ans
             List<Integer> newLeaves = new ArrayList<>();
-            for (int i : leaves) {
-                int j = adj.get(i).iterator().next();
-                adj.get(j).remove(i);
-                if (adj.get(j).size() == 1) newLeaves.add(j);
+            for(int i : leaves){
+                //get the next adjacent node and remove curr node from it's list 
+                int nextNode = adj.get(i).iterator().next();
+                adj.get(nextNode).remove(i); 
+                if(adj.get(nextNode).size() == 1) newLeaves.add(nextNode);
             }
             leaves = newLeaves;
         }
