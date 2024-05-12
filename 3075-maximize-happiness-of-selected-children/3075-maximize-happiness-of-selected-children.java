9120f46370782a520a57bdcde82bb1e5f8c40b09
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-05-12 16:37:18 +0530

    Time: 142 ms (14.75%), Space: 74 MB (13.19%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -2,23 +2,22 @@ class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
         // Create a max heap using PriorityQueue with a custom comparator
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-
+        
         // Add all elements to the priority queue
-        for (int h : happiness) {
-            pq.add(h);
+        for(int i : happiness){
+            pq.add(i); 
         }
-
-        long totalHappinessSum = 0;
+        
+        long sum = 0;
         int turns = 0;
-
-        for (int i = 0; i < k; i++) {
+        
+        for(int i=0; i<k; i++){
             // Add the current highest value to the total happiness sum and remove it from the max heap 
-            totalHappinessSum += Math.max(pq.poll() - turns, 0);
-
+            sum += Math.max(pq.poll() - turns, 0); 
             // Increment turns for the next iteration
             turns++;
         }
-
-        return totalHappinessSum;
+        
+        return sum;
     }
 }
