3d8295e50cbebe4e74eb903ba69bbb05b7ff0e1f
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-05-12 17:32:00 +0530

    Time: 753 ms (12.59%), Space: 69.1 MB (35.28%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        //sort as per pairs fraction in ascending order
         PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingDouble(a -> (double) a[0] / a[1]));
-        int N = arr.length;
         
-        for (int i = 0; i < N; ++i) {
-            for (int j = i + 1; j < N; ++j) {
-                minHeap.offer(new int[]{arr[i], arr[j]});
-            }
+        int n = arr.length;
+        
+        //insert all pairs into pq
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                 minHeap.add(new int[]{arr[i], arr[j]});
+            }     
         }
         
         int[] result = null;
-        for (int i = 0; i < k; ++i) {
+        //pick kth element from pair
+        for(int i=0; i<k; ++i){
             result = minHeap.poll();
         }
         
