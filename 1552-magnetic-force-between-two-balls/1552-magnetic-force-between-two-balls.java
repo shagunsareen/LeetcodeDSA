903d5cbbb5fe383dd2dac8abbf37d92ee5bbfd69
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-06-22 03:21:37 +0530

    Time: 40 ms (68.49%), Space: 56.3 MB (6.00%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,30 +1,37 @@
 class Solution {
     public int maxDistance(int[] position, int m) {
+        //this is binary search on answer
         Arrays.sort(position);
-        int lo = 0;
-        int hi = position[position.length-1];
-        int optimal = 0;
-        while (lo <= hi) {
-            int mid = lo + (hi-lo)/2;
-            if (isPlacementPossible(position, m, mid)) {
-                optimal = mid;
-                lo = mid+1;
-            } else {
-                hi = mid-1;
+        int low = 0;
+        int high = position[position.length-1];
+        int ans = 0;
+        
+        while(low<=high){
+            int mid = (low + high)/2;
+            
+            if(isPlacementPossible(position, m , mid)){
+                ans = mid;
+                //since we have to maximize the ans we will increase the low 
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
-        }
-        return optimal;
+        } 
+       return ans;
     }
-
-    private boolean isPlacementPossible(int[] positions, int m, int max) {
-        int count = 1;
-        int last = positions[0];
-        for (int i = 0; i < positions.length; i++) {
-            if (positions[i] - last >= max) {
-                last = positions[i];
+    
+    
+    private boolean isPlacementPossible(int[] position, int m, int mid){
+        //we have to check whether the max distance we have passed as input will place all the balls or not
+        int count = 1; //since first ball we will place at 0th index anyway
+        int last = position[0];
+        for(int i=0; i<position.length; i++){
+            //check the distance between the 2 points and if it is greater or equal to the decided value 
+            if(position[i] - last >= mid){
                 count++;
+                last = position[i];
             }
         }
-        return count >= m;
+        return count >=m; //if we are able to place all the balls in the decided distance then return true
     }
 }
