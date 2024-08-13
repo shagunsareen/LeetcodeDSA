fd0ed660f50e8dada06f859695e743f95cae11c6
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-08-14 04:13:02 +0530

    Time: 1 ms (94.12%), Space: 44.8 MB (11.08%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,5 +1,5 @@
 class Solution {
-    List<List<Integer>> res;
+    /*List<List<Integer>> res;
     
     public List<List<Integer>> permute(int[] nums) {
         res = new ArrayList<>();
@@ -23,5 +23,33 @@ class Solution {
                 subres.remove(subres.size() - 1); //remove last element inserted 
             }
         }
-    }       
+    }
+    */
+    
+    List<List<Integer>> result;
+    public List<List<Integer>> permute(int[] nums){
+        result = new ArrayList<>();
+        ArrayList<Integer> currentList = new ArrayList<>();
+        getPermutations(currentList, nums);
+        return result;
+    }
+    
+    private void getPermutations(ArrayList<Integer> currentList, int[] nums){  
+        //base case
+        if(currentList.size() == nums.length){
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+        
+        
+        for(int i=0; i<nums.length; i++){
+            //check if the currentList has the same digit or not
+            if(!currentList.contains(nums[i])){
+                currentList.add(nums[i]);
+                getPermutations(currentList, nums);
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+            
 }
