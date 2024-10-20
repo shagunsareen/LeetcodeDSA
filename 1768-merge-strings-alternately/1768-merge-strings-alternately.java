e22e92438f2ef658ac89085035979394dc08968c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-10-20 16:49:19 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (35.40%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+        int l1 = word1.length();
+        int l2 = word2.length();
+        
+        int p1 = 0;
+        int p2 = 0;
+        
+        StringBuilder str = new StringBuilder();
+        
+        while(p1 < l1 && p2 < l2){
+            str.append(word1.charAt(p1)).append(word2.charAt(p2));
+            p1++;
+            p2++;
+        }
+    
+        if(p1 < l1){
+            str.append(word1.substring(p1, l1));
+        }
+        
+        if(p2 < l2){
+            str.append(word2.substring(p2, l2));
+        }
+        
+        return str.toString();
+    }
+}
