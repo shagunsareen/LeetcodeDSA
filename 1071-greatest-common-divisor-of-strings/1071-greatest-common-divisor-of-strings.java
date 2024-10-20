a9be438e1b36710b3dd0c47d378c093e4d5084a0
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-10-20 18:05:57 +0530

    Time: 1 ms (70.60%), Space: 41.5 MB (98.88%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,21 +1,22 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        //check whether both string additions are equal or not        
-        if(!(str1+str2).equals(str2+str1)){
+    public String gcdOfStrings(String str1, String str2) {     
+        if(!(str1+str2).equals(str2+str1)){ //no GCD exists
             return "";
         }else{
-            int gcd = getGCD(str1.length() , str2.length());
-            return str1.substring(0, gcd);
-        }
+            //if it exists then find the GCD string among them
+            // if strings on combo are equal then GCDString should have length of GCD of both strings length
+            int l1 = str1.length();
+            int l2 = str2.length();
+            
+            return str1.substring(0, getGCD(l1, l2));
+        } 
     }
     
-    private int getGCD(int x, int y)
-    {
-        if(y==0){ 
-            return x; 
-        }
-        else{
-            return getGCD(y,x%y); 
+    private int getGCD(int x, int y){
+        if(y==0){
+            return x;
+        }else{
+            return getGCD(y, x % y);
         }
     }
 }
