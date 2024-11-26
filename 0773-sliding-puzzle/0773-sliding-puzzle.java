95f2240e8a13a05047f9495610839c05406a722d
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-11-26 11:04:24 +0530

    Time: 6 ms (97.49%), Space: 43.8 MB (56.44%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.java
+++ 0773-sliding-puzzle/0773-sliding-puzzle.java
@@ -1,63 +1,75 @@
 class Solution {
 
     public int slidingPuzzle(int[][] board) {
-        // Direction map for zero's possible moves in a 1D representation of the 2x3 board
-        int[][] directions = new int[][] {
-            { 1, 3 },
-            { 0, 2, 4 },
-            { 1, 5 },
-            { 0, 4 },
-            { 1, 3, 5 },
-            { 2, 4 },
-        };
-
         String target = "123450";
-        StringBuilder startState = new StringBuilder();
-
-        // Convert the 2D board into a string representation
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        
+        //1. collect all possible adjacent positions where the 0 can go if it is at that particular index
+        int[][] directions = new int[][]{
+            {1,3}, 
+            {0,2,4}, 
+            {1,5},
+            {0,4},
+            {1,3,5},
+            {2,4}
+        };
+        
+        
+        StringBuilder startState = new StringBuilder();  
+        //we have to understand this problem as moving from start point to the end point. Start state will keep on changing till we reach the end point and if we don't reach end point we return -1
+        
+        //convert 2D array to string rep
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
                 startState.append(board[i][j]);
             }
         }
-
-        Set<String> visited = new HashSet<>(); // To store visited states
+        
+        //put this startState to the queue and mark it as visited 
         Queue<String> queue = new LinkedList<>();
-        queue.add(startState.toString());
+        Set<String> visited = new HashSet<>();
         visited.add(startState.toString());
-
-        int moves = 0;
-
-        // BFS to find the minimum number of moves
-        while (!queue.isEmpty()) {
+        queue.add(startState.toString());
+        
+        int moves=0;
+        
+        
+        while(!queue.isEmpty()){
             int size = queue.size();
-            while (size-- > 0) {
-                String currentState = queue.poll();
-
-                // Check if we reached the target solved state
-                if (currentState.equals(target)) {
+            
+            while(size-- != 0){
+                
+                String currState = queue.poll();
+                
+                //check if this is same as target 
+                if(currState.equals(target)){
                     return moves;
                 }
-
-                int zeroPos = currentState.indexOf('0');
-                for (int newPos : directions[zeroPos]) {
-                    String nextState = swap(currentState, zeroPos, newPos);
-
-                    // Skip if this state has been visited
-                    if (visited.contains(nextState)) continue;
-
-                    // Mark the new state as visited and add it to the queue
+                
+                //if it is not then go and check adjacent positions to swap 0 
+                int zeroPos = currState.indexOf('0');
+                
+                //get adjacent elements of zeroPos
+                for(int newPos : directions[zeroPos]){
+                    
+                    //swap 
+                    String nextState = swap(currState, zeroPos, newPos);
+                    
+                    //push to queue if not visited
+                    if(visited.contains(nextState)) continue;
+                    
+                    ///else mark it visited and push to queue
                     visited.add(nextState);
                     queue.add(nextState);
+                    
                 }
             }
             moves++;
-        }
-        return -1;
+        }     
+       return -1;
     }
-
-    // Helper method to swap characters at indices i and j in the string
-    private String swap(String str, int i, int j) {
+    
+    //swap chars at both indices 
+    private String swap(String str, int i, int j){
         StringBuilder sb = new StringBuilder(str);
         sb.setCharAt(i, str.charAt(j));
         sb.setCharAt(j, str.charAt(i));
