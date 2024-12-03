178af6e08ca721ae00d36aa2d5907713b81dcda9
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-12-04 01:51:57 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (43.62%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,20 +1,16 @@
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
-        // Split the sentence into words
         String[] words = sentence.split(" ");
-        // Iterate over the words with their positions
-        for (int i = 0; i < words.length; i++) {
-            // Check if the current word is long enough to contain the searchWord as a prefix
-            // and if the prefix matches the searchWord
-            if (
-                words[i].length() >= searchWord.length() &&
-                words[i].substring(0, searchWord.length()).equals(searchWord)
-            ) {
-                // If a match is found, return the current word position (adjusted to 1-based index)
-                return i + 1;
+        int searchWLen = searchWord.length();
+        
+        for(int i=0; i<words.length; i++){
+            int currWLen = words[i].length();
+            
+            if(!(searchWLen > currWLen) && words[i].substring(0, searchWLen).equals(searchWord)){
+                return i+1;
             }
         }
-        // If no match is found, return -1
+        
         return -1;
     }
 }
