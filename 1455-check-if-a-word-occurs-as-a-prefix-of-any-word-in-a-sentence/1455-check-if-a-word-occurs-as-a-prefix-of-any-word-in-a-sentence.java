85bb6c6e9a437340b826c50769bd60ad5740aaf5
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2024-12-04 02:43:56 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (74.82%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,6 +1,8 @@
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
-        String[] words = sentence.split(" ");
+        
+        //Approach : TC:O(n), SC:O(n)
+        /*String[] words = sentence.split(" ");
         int searchWLen = searchWord.length();
         
         for(int i=0; i<words.length; i++){
@@ -9,6 +11,41 @@ class Solution {
             if(!(searchWLen > currWLen) && words[i].substring(0, searchWLen).equals(searchWord)){
                 return i+1;
             }
+        }
+        
+        return -1;*/
+        
+        //Approach : TC:O(n), SC:O(1)
+        int strPtr = 0;
+        int wordIndex = 1;
+        
+        //check if str char is same as word char 
+        while(strPtr < sentence.length())
+        {
+            if(strPtr == 0 || sentence.charAt(strPtr-1) == ' ')
+            {   //check for only starting indexes and then move forward
+                //int tmpPtr = strPtr;
+                int wordPtr = 0;
+                int matchedCharCount = 0;
+
+                while(wordPtr < searchWord.length() && strPtr < sentence.length() && sentence.charAt(strPtr) == searchWord.charAt(wordPtr))
+                {
+                    wordPtr++;
+                    strPtr++;
+                    //tmpPtr++;
+                    matchedCharCount++;
+
+                    if(matchedCharCount == searchWord.length()){
+                        return wordIndex;
+                    }
+                } 
+            }
+            
+            if(strPtr < sentence.length() && sentence.charAt(strPtr) == ' '){
+                wordIndex++;
+            }
+            
+            strPtr++;      
         }
         
         return -1;
