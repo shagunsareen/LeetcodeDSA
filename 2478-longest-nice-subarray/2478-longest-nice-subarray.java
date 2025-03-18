ffdb701ea0259b60f8c7a70c34330d2a8c75d7ca
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-03-19 01:14:35 +0530

    Time: 3 ms (98.67%), Space: 55.9 MB (34.22%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+
+        // 2-Pointer: Variable size sliding window
+        int maxWindowSize = 0;
+        int xorSum = 0;
+        int currSum = 0;
+
+        while (right < n) {
+            currSum += nums[right];
+            xorSum ^= nums[right];
+
+            while (xorSum != currSum) {
+                currSum -= nums[left];
+                xorSum ^= nums[left];
+                left++;
+            }
+
+            maxWindowSize = Math.max(maxWindowSize, right - left + 1);
+            right++;
+        }
+
+        return maxWindowSize;
+    }
+}
