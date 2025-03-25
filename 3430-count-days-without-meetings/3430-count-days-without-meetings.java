5028a0feef64cd2ff1de87b38d6ee66a0e8796cc
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-03-25 18:54:27 +0530

    Time: 44 ms (28.15%), Space: 100.5 MB (55.63%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        int freeDays = 0, latestEnd = 0;
+        /*int freeDays = 0, latestEnd = 0;
 
         // Sort meetings based on starting times
         Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
@@ -20,6 +20,28 @@ class Solution {
         // Add all days after the last day of meetings
         freeDays += days - latestEnd;
 
-        return freeDays;
+        return freeDays;*/
+
+        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0], b[0]));
+
+        int count = 0;
+        int daysWorked = 0;
+        int n = meetings.length;
+
+        for(int i=0; i<n; i++){
+            int start = meetings[i][0];
+            int end = meetings[i][1];
+
+            while(i+1 < n && end >= meetings[i+1][0]){ //meaning we have overlapping intervals
+                end = Math.max(end, meetings[i+1][1]);    
+                i++;
+            }
+
+            daysWorked += end - start + 1;
+            System.out.println(daysWorked);
+        }
+
+        count = days - daysWorked;
+        return count;
     }
 }
