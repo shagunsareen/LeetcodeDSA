6b2e6888e489f748505c6fd0b11c4c6c40dafe9c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-03-25 19:39:51 +0530

    Time: 103 ms (14.57%), Space: 85.4 MB (100%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,29 +1,7 @@
 class Solution {
-    public int countDays(int days, int[][] meetings) {
-        /*int freeDays = 0, latestEnd = 0;
-
-        // Sort meetings based on starting times
-        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
-
-        for (int[] meeting : meetings) {
-            int start = meeting[0], end = meeting[1];
-
-            // Add current range of days without a meeting
-            if (start > latestEnd + 1) {
-                freeDays += start - latestEnd - 1;
-            }
-
-            // Update latest meeting end
-            latestEnd = Math.max(latestEnd, end);
-        }
-
-        // Add all days after the last day of meetings
-        freeDays += days - latestEnd;
-
-        return freeDays;*/
-
-        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0], b[0]));
-
+    public int countDays(int days, int[][] meetings) {     
+        //Merge intervals approach 
+       /*Arrays.sort(meetings, (a,b) -> Integer.compare(a[0], b[0]));
         int count = 0;
         int daysWorked = 0;
         int n = meetings.length;
@@ -36,12 +14,36 @@ class Solution {
                 end = Math.max(end, meetings[i+1][1]);    
                 i++;
             }
-
             daysWorked += end - start + 1;
-            System.out.println(daysWorked);
         }
-
         count = days - daysWorked;
+        return count; */
+
+        //Approach 2 : Line sweep algo   
+        //split every interval to +1 when it starts and -1 when it ends so that if prefix sum is taken it gives correct count of meeting during that interval 
+        List<int[]> time = new ArrayList<>();
+        for(int[] meeting : meetings){
+            time.add(new int[]{meeting[0], 1}); //entering bus example
+            time.add(new int[]{meeting[1] + 1, 0}); //leaving room example
+        }
+        
+        //Sort it as per start time
+        time.sort((a,b) -> Integer.compare(a[0], b[0]));
+        
+        //Apply Line-sweep 1D
+        int overlap = 0;
+        int count = time.get(0)[0] - 1; //count free time before start
+
+        for(int i=0; i<time.size()-1; i++){ //we will check till second last interval since we will handle last interval separately
+            if(time.get(i)[1] == 0) overlap--;
+            else overlap++;
+
+            if(overlap == 0){
+                //means there are no overlap meetings 
+                count += time.get(i+1)[0] - time.get(i)[0];
+            }
+        }
+        count += days - time.get(time.size() - 1)[0] + 1;
         return count;
     }
 }
