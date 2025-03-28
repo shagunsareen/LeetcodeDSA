484e3ab9ff5f862488111a439312d57b4e55f0d0
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-03-28 16:44:26 +0530

    Time: 12 ms (78.98%), Space: 56.7 MB (81.53%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,40 +1,50 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        // Find the majority element
-        int x = nums.get(0), count = 0, xCount = 0, n = nums.size();
+        //step 1 : find majority element using moore's voting algo
+        int majorityEle = nums.get(0); //assume 
+        int currCount = 0; //this will determine if there are more elements in nums than majorityElement
 
-        for (int num : nums) {
-            if (num == x) {
-                count++;
-            } else {
-                count--;
+        for(int num : nums){
+            if(num == majorityEle){
+                currCount++;
+            }else{
+                currCount--;
             }
-            if (count == 0) {
-                x = num;
-                count = 1;
+
+            if(currCount == 0){
+                //meaning other elements are more compared to the majorityElement so consider curr element as majorityelement
+                majorityEle = num;
+                currCount = 1; 
             }
         }
 
-        // Count frequency of majority element
-        for (int num : nums) {
-            if (num == x) {
-                xCount++;
+        //after this for loop we will get majorityElement but we need to double check whether its freq is actually more than all other elements
+
+        //step2 : check the frequency of majority element
+        int freq = 0;
+        for(int num : nums){
+            if(num == majorityEle){
+                freq++;
             }
         }
 
-        // Check if valid split is possible
-        count = 0;
-        for (int index = 0; index < n; index++) {
-            if (nums.get(index) == x) {
-                count++;
+        //step3 : check if valid split is possible with this majorityElement
+        int prefixCount = 0;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            //we will check how many times curr majorityele occurs so that we can check if we split it at i index will it be me in both split arrays or not
+            if(nums.get(i) == majorityEle){
+                prefixCount++;
             }
-            int remainingCount = xCount - count;
-            // Check if both left and right partitions satisfy the condition
-            if (count * 2 > index + 1 && remainingCount * 2 > n - index - 1) {
-                return index;
+
+            int remainingFreq = freq - prefixCount;
+            //elements on left side of split are b-a+1 i.e. i-0+1 = i+1 
+            //elements on right side of split are n-1-(i+1)+1 = n-i-1
+            if(prefixCount * 2 > i + 1 && remainingFreq * 2 > n-i-1){
+                return i; //valid split
             }
         }
-
         return -1;
     }
 }
