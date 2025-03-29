eba1c2ffa46e80b74046b4d54a82d50bb29e69c5
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-03-29 18:53:36 +0530

    Time: 97 ms (64.66%), Space: 59.1 MB (36.09%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.java
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.java
@@ -1,69 +1,78 @@
 import java.util.*;
 
 class Solution {
-    static class Pair {
-        int first, second;
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
-    static class Triple {
-        int first;
-        Pair second;
-        Triple(int first, Pair second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
-    boolean isValid(int x, int y, int m, int n) {
-        return x >= 0 && x < m && y >= 0 && y < n;
-    }
-
     public int[] maxPoints(int[][] grid, int[] queries) {
-        Set<Integer> sortedQueries = new TreeSet<>();
-        for (int query : queries) sortedQueries.add(query);
+        int qsize = queries.length;
+        int[] result = new int[qsize];
 
-        Map<Integer, Integer> queryCount = new HashMap<>();
-        PriorityQueue<Triple> minheap = new PriorityQueue<>((a, b) -> a.first - b.first);
-        int m = grid.length, n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
+        //we need sorted queries since we will use points calculated for lower value integers for higher valued integers but we need to return answer in input order so we store queries with index in an array
+        
+        int[][] sortedQueries = new int[qsize][2];
+        for(int i=0; i<qsize; i++){
+            sortedQueries[i][0] = queries[i]; //element
+            sortedQueries[i][1] = i; //storing original index of queries
+        }
 
-        minheap.add(new Triple(grid[0][0], new Pair(0, 0)));
+        Arrays.sort(sortedQueries, (a,b) -> a[0] - b[0]);
+
+        //min heap will process cells in increasing order of value
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[0] - b[0]); //this is needed to keep track of cells that can be visited next to consider points
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+
+        //insert the top left cell as its given in question that we need to start from top left cell
+        // curr val, curr row, curr col
+        minHeap.add(new int[]{grid[0][0], 0, 0});
         visited[0][0] = true;
-        int[] dir = {-1, 0, 1, 0, -1};
 
+        int[][] dir = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
         int count = 0;
-        for (int query : sortedQueries) {
-            while (!minheap.isEmpty()) {
-                Triple curr = minheap.poll();
-                int val = curr.first;
-                int x = curr.second.first;
-                int y = curr.second.second;
-                if (val >= query) {
-                    minheap.add(curr);
-                    break;
+
+        //process for all queries 
+        for(int[] query : sortedQueries){
+            int queryValue = query[0];
+            int queryIndex = query[1];
+
+            while(!minHeap.isEmpty()){
+                int[] curr = minHeap.poll();
+                int cellValue = curr[0];
+                int cellRow = curr[1];
+                int cellCol = curr[2];
+
+                if(cellValue >= queryValue){
+                    minHeap.add(curr); //since this doesn't meet queryval condition it might meet other queryval condition
+                    break; 
                 }
+
+                //else count this cell as valid
                 count++;
 
-                for (int i = 0; i < 4; i++) {
-                    int newX = x + dir[i];
-                    int newY = y + dir[i + 1];
-                    if (isValid(newX, newY, m, n) && !visited[newX][newY]) {
-                        visited[newX][newY] = true;
-                        minheap.add(new Triple(grid[newX][newY], new Pair(newX, newY)));
+                //check all neighbours 
+                for(int[] direction : dir){
+                    int newRow = cellRow + direction[0];
+                    int newCol = cellCol + direction[1];
+
+                    //check if it is valid and not visited, then push the neighbour to minheap
+                    if(isValid(newRow, newCol, rows, cols) && !visited[newRow][newCol]){
+                        //value , row, col
+                        minHeap.add(new int[]{grid[newRow][newCol], newRow, newCol});
+                        //mark them visited
+                        visited[newRow][newCol] = true; 
                     }
-                }
+                }  
             }
-            queryCount.put(query, count);
+
+            //store result for this query
+            result[queryIndex] = count;
         }
 
-        int[] res = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            res[i] = queryCount.get(queries[i]);
-        }
-        return res;
+        return result;
     }
+
+    private boolean isValid(int newRow, int newCol, int rows, int cols){
+        return newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols;
+    }
+
 }
