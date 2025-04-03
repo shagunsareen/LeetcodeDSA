a6a563a25eb238caea459743b3e238228c3719b0
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-04-04 00:01:21 +0530

    Time: 5 ms (28.63%), Space: 47.6 MB (69.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -45,7 +45,7 @@ class Solution {
     */
 
     //Approach 2 : Tabulation
-    int[][] dp;
+    /*int[][] dp;
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
@@ -74,5 +74,42 @@ class Solution {
             }
         }
         return dp[n-1][m-1];
+    }*/
+
+    //Approach 3 : Space optimisation
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[] prev = new int[m];
+
+        for(int i=0; i<n; i++){ 
+            int[] temp = new int[m]; //this will have curr row values 
+            for(int j=0; j<m; j++){
+                
+                if(i == 0 && j == 0){
+                    temp[j] = grid[i][j]; 
+                }else{
+                    int up = grid[i][j];
+                    int left = grid[i][j];
+
+                    if(i > 0){
+                        up += prev[j];
+                    }else{
+                        up = Integer.MAX_VALUE;
+                    }
+                    if(j > 0){
+                        left += temp[j-1];
+                    }else{
+                        left = Integer.MAX_VALUE;
+                    }
+
+                    temp[j] = Math.min(up, left);
+                }
+            }
+            prev = temp; // so that prev should have curr value as prev for next iteration
+        }
+
+        return prev[m-1];
     }
 }
