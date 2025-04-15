2e7dbe9fe3e46bb22e2e5a3c1426e0e7e4c0c697
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-04-15 18:56:39 +0530

    Time: 5 ms (86.86%), Space: 43.1 MB (53.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,122 +1,51 @@
 class Solution {
-    
-    
-    //Approach 1 : Recursion 
-    /*int totalWays = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        calculateWays(nums, target, 0, 0);
-        return totalWays;
+        //get the totalSum 
+        int n = nums.length;
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        }
+
+        if (totalSum - target < 0 || (totalSum - target) % 2 == 1)
+            return 0;
+
+        //we have to find subsets with (totalSum - target)/2 diff 
+        int s2Sum = (totalSum - target)/2;
+        int s1Sum = totalSum - s2Sum;
+
+        return countPartitions(nums, s2Sum);
     }
-    
-    private void calculateWays(int[] num, int target, int currIndex, int currSum){
-        //base case where to stop 
-        if(currIndex == num.length){
-            //check target
-            if(currSum == target)
-                totalWays++;
+
+    int[][] dp;
+    private int countPartitions(int[] nums, int sum){
+        int n = nums.length;
+        dp = new int[n][sum + 1]; // n - index , target + 1 - sum at each col
+
+        //0th row 
+        if(nums[0] == 0){
+            dp[0][0] = 2; //either take 0 or not sum is achieved
         }else{
-           calculateWays(num, target, currIndex+1, currSum + num[currIndex]); //take positive integer
-           calculateWays(num, target, currIndex+1, currSum - num[currIndex]); //take negative integer
-        }
-    }*/
-    
-    
-    //Approach 2 : Recursion with Memoization 
-    /*int totalSum;
-    public int findTargetSumWays(int[] nums, int target) {  
-        totalSum = Arrays.stream(nums).sum();
-        
-        int[][] memo = new int[nums.length][2*totalSum + 1];
-        for(int[] row : memo){
-            Arrays.fill(row, Integer.MIN_VALUE);
-        } 
-        
-        return calculateTotalWays(nums, target, 0, 0, memo);
-    }
-    
-    private int calculateTotalWays(int[] nums, int target, int currIndex, int currSum, int[][] memo)
-    {
-          //base case if we have reached end index
-          if(currIndex == nums.length){
-              if(currSum == target){
-                  return 1;
-              }else{
-                  return 0;
-              }
-          }else{
-              //check if the result is already computed 
-              if(memo[currIndex][currSum + totalSum] != Integer.MIN_VALUE){
-                  return memo[currIndex][currSum + totalSum];
-              }
-              
-              //include the positive of the current number
-              int add = calculateTotalWays(nums, currIndex + 1, currSum + nums[currIndex], target, memo);
-              
-              // include the negative of the current number 
-              int subtract = calculateTotalWays(nums, currIndex + 1, currSum - nums[currIndex], target, memo);
-              
-              //Store the result in the memoization table
-              memo[currIndex][currSum + totalSum] = add + subtract;  
-              return memo[currIndex][currSum + totalSum];
-          }
-    }*/
-
-    // Sum of all elements in the array
-    int totalSum;
-
-    public int findTargetSumWays(int[] nums, int target) {
-        totalSum = Arrays.stream(nums).sum();
-
-        int[][] memo = new int[nums.length][2 * totalSum + 1];
-        for (int[] row : memo) {
-            Arrays.fill(row, Integer.MIN_VALUE);
-        }
-        return calculateWays(nums, 0, 0, target, memo);
-    }
-
-    private int calculateWays(
-        int[] nums,
-        int currentIndex,
-        int currentSum,
-        int target,
-        int[][] memo
-    ) {
-        if (currentIndex == nums.length) {
-            // Check if the current sum matches the target
-            if (currentSum == target) {
-                return 1;
-            } else {
-                return 0;
+            dp[0][0] = 1; //not pick 
+            if(nums[0] <= sum){ //meaning we can pick element at 0th index since its value is less than the target
+                dp[0][nums[0]] = 1;
             }
-        } else {
-            // Check if the result is already computed
-            if (
-                memo[currentIndex][currentSum + totalSum] != Integer.MIN_VALUE
-            ) {
-                return memo[currentIndex][currentSum + totalSum];
-            }
-            // Calculate ways by adding the current number
-            int add = calculateWays(
-                nums,
-                currentIndex + 1,
-                currentSum + nums[currentIndex],
-                target,
-                memo
-            );
-
-            // Calculate ways by subtracting the current number
-            int subtract = calculateWays(
-                nums,
-                currentIndex + 1,
-                currentSum - nums[currentIndex],
-                target,
-                memo
-            );
-
-            // Store the result in memoization table
-            memo[currentIndex][currentSum + totalSum] = add + subtract;
-
-            return memo[currentIndex][currentSum + totalSum];
         }
+
+        for(int index = 1; index < n; index++){
+            for(int target = 0; target <= sum; target++){
+
+                int notPick = dp[index-1][target];
+
+                int pick = 0;
+
+                if(nums[index] <= target){
+                    pick = dp[index-1][target - nums[index]];
+                }
+
+                dp[index][target] = pick + notPick;
+            }
+        }
+        return dp[n-1][sum];
     }
 }
