6a92f6b58338993d072127ca34f6a5b841da5eec
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-04-16 19:11:22 +0530

    Time: 9 ms (63.5%), Space: 49.2 MB (44.83%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[][] dp;
+    public int change(int amount, int[] coins) { 
+        //in this dp array we will store using elements from 0 to ith index in how many ways target sum can be achieved 
+        int n = coins.length;
+        dp = new int[n][amount + 1];
+
+        //base row 
+        for(int col = 0; col <= amount; col++){
+            if(col % coins[0] == 0){
+                dp[0][col] = 1; // we are storing no. of ways target can be achieved not the no. of coins used
+            }else{
+                dp[0][col] = 0;
+            }
+        }
+
+        //recursion
+        for(int index = 1; index < n; index++){
+            for(int target = 0; target <= amount; target++){
+                int pick = 0;
+
+                if(coins[index] <= target){
+                    pick = dp[index][target - coins[index]]; //pick same element multiple times 
+                }
+
+                int notPick = dp[index-1][target]; //not pick so move to previous element
+
+                dp[index][target] = pick + notPick; //ways to achieve target
+            }
+        }
+
+        return dp[n-1][amount];
+    }
+}
