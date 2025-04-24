43fe419bd4564001d007cb7320bee403cfb2424c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-04-25 00:29:05 +0530

    Time: 11 ms (34.44%), Space: 45.1 MB (47.27%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
+        /*Approach 1
         int wholeCount = 0;
         Set<Integer> set = new HashSet<>();
         for(int num : nums){
@@ -24,6 +25,38 @@ class Solution {
                 }
             }
         }    
+        return ans;*/
+
+        //Approach 2
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int distinct = set.size();
+        int right = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        int n = nums.length;
+        
+        for(int left = 0; left < n; left++){
+            while(right < n && map.size() < distinct){
+                int curr = nums[right];
+                map.put(curr, map.getOrDefault(curr, 0) + 1);
+                right++;
+            }
+
+            if(map.size() == distinct){
+                ans += n - right + 1; //subarrays from valid window to end are all valid
+            }
+
+            //remove left
+            int remove = nums[left];
+            map.put(remove, map.get(remove) - 1);
+            if(map.get(remove) == 0){
+                map.remove(remove);
+            }
+        }
         return ans;
     }
 }
