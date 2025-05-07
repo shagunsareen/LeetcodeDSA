f7f026c041bf22a6c1b273f16299816aaf6d0fa1
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-08 01:16:05 +0530

    Time: 1 ms (62.54%), Space: 46 MB (81.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,34 +1,70 @@
 class Solution {
     public int trap(int[] height) {
         
-        //we need leftMax and rightMax at every point 
-        int n = height.length;
-        int leftMax = Integer.MIN_VALUE;
-        int rightMax = Integer.MIN_VALUE;
+        //Approach 1 : SC :O(2n)
         
-        //create leftMax array 
-        int[] leftMaxArr = new int[n];
-        for(int i=0; i<n; i++){
-            if(leftMax < height[i])
-                leftMax = height[i];
-            leftMaxArr[i] = leftMax;
+        /*int n = height.length;
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+
+        int[] rightMax = new int[n];
+        rightMax[n-1] = height[n-1];
+
+        // iterate to get left max
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
         }
-        
-        //create rightMax array
-        int[] rightMaxArr = new int[n];
-        for(int i=n-1; i>=0; i--){
-            if(rightMax < height[i])
-                rightMax = height[i];
-            rightMaxArr[i] = rightMax;
+
+        //iterate to get right max
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
         }
-        
-        //now we need to calculate the water trapped
-        // water trapped can be min of max leftheight and max rightheight, we need to subtract actual height of a place since that will decrease the water that can be trapped 
-        int waterTrapped = 0;
+
+        int totalWaterTrapped = 0;
+
+        //now calculate the water trapped
         for(int i=1; i<n-1; i++){
-            waterTrapped += Math.max(Math.min(leftMaxArr[i-1], rightMaxArr[i+1]) - height[i], 0);
+            totalWaterTrapped += Math.max((Math.min(leftMax[i-1], rightMax[i+1]) - height[i]),0);
+            System.out.println("LM : "+leftMax[i]+ ", RM : "+rightMax[i]+", Ht : "+height[i]+",WT: "+totalWaterTrapped);
         }
-        
-        return waterTrapped;
+
+        return totalWaterTrapped;*/
+
+        int n = height.length;
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        int[] rightMax = new int[n];
+        rightMax[n-1] = height[n-1];
+
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i-1]);
+        }
+
+        for(int j=n-2; j>=0; j--){
+            rightMax[j] = Math.max(rightMax[j+1], height[j+1]);
+        }
+
+        int totalWater = 0;
+        for(int i=1; i<n-1; i++){
+            totalWater += Math.max(Math.min(leftMax[i],rightMax[i]) - height[i], 0);
+        }
+
+        return totalWater;
+
+        //Approach 2 : without arrays
+        /*int n = height.length;
+        int lmax =height[0];
+        int rmax =height[n-1];
+        int totalWater = 0;
+        int l =0;
+        int r =n-1;
+
+        while(l<r){
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+            totalWater+=(lmax<rmax) ? lmax-height[l++] : rmax-height[r--];
+        }
+
+        return totalWater;*/
     }
 }
