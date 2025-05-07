10241b32a40a99131ab47fed797f86ede1e0d9ed
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-07 20:25:03 +0530

    Time: 4 ms (48.59%), Space: 48.9 MB (93.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,47 @@
 class Solution {
-    
-    private int n;
-    private int m;
-    
-    public int numIslands(char[][] grid){
+    int[][] directions = new int[][]{{0,1}, {0,-1}, {-1, 0}, {1,0}};
+
+    public int numIslands(char[][] grid) {
         int count = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-        
-        //select the starting point and see how many starting point we can have that many no. of distinct islands 
-        //because that would mean 1 island is not covering every cell and has some unvisited cells still
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j] == '1'){ 
-                    //take this as the starting point 
-                    count++; //this is one island 
-                    dfs(grid, i, j, row, col);
-            }
-         }
-       }
-      return count;
-    }
-    
-    private void dfs(char[][] A, int i, int j, int n , int m){
-        //base case - if we have reached water or out of bounds then return from this path
-        if(i < 0 || j < 0 || i>=n || j>=m || A[i][j] == '0'){
-            return; //current connected island is over
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] visited = new boolean[n][m];
+        for(boolean[] arr : visited){
+            Arrays.fill(arr, false);
         }
-        
-        //now mark the current cell as visited 
-        A[i][j] = '0';
-        
-        //go in all directions and mark them as visited so that we should not count them again in same island 
-        dfs(A, i+1, j, n, m);
-        dfs(A, i-1, j, n, m);
-        dfs(A, i, j+1, n, m);
-        dfs(A, i, j-1, n, m);
+
+        //iterate over grid and find cell with 1 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                   bfs(i, j, visited, grid);
+                   count++;
+                }
+            }
+        }
+        return count;
     }
-    
-}   
-    
+
+    private void bfs(int row, int col, boolean[][] visited, char[][] grid){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length){
+            return;
+        }
+
+        if(visited[row][col] || grid[row][col] == '0'){
+            return;
+        }
+
+        //mark it visited 
+        visited[row][col] = true;
+
+        //go to all neighbours
+        for(int[] dir : directions){
+            int x1 = row + dir[0];
+            int y1 = col + dir[1];
+
+            bfs(x1, y1, visited, grid);
+        }
+    }
+
+}
