860c0ea28dc7126b9c926e88f2a74d4bff6a8a6c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-19 22:51:50 +0530

    Time: 134 ms (43.9%), Space: 55.2 MB (9.76%) - LeetHub

--- 2061-painting-a-grid-with-three-different-colors/2061-painting-a-grid-with-three-different-colors.java
+++ 2061-painting-a-grid-with-three-different-colors/2061-painting-a-grid-with-three-different-colors.java
@@ -1,34 +1,43 @@
 class Solution {
-    private static final int MOD = (int)1e9 + 7;
-    private int[][] state_mem = new int[1002][1024]; // 1000 rows + 2, 1024 for 10 bits
-    
+   
+    private static final int MOD = 1000000007;
+    private int[][] dp = new int[1002][1024]; //dp array stores at which column how many states are possible to color, possible states range from 0 to 1024 since in each column we will have max 5 rows so 5 colors we can keep and if each color corresponds to 2 bits then its 10 bits with which we can make 1024 combinations
+
     public int colorTheGrid(int m, int n) {
-        for (int i = 0; i < state_mem.length; i++) {
-            Arrays.fill(state_mem[i], -1);
+        
+        //fill dp array with -1
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
+
+        //return no. of ways in which we can color valid combinations
         return countWays(m, n, 0, 0, 0, 0);
     }
-    
-    private int countWays(int m, int n, int r, int c, int curr_state, int prev_state) {
-        if (c == n) return 1;
-        if (r == m) return countWays(m, n, 0, c + 1, 0, curr_state);
-        if (r == 0 && state_mem[c][prev_state] != -1) return state_mem[c][prev_state];
-        
+
+    private int countWays(int m, int n, int r, int c, int curr_state, int prev_state){
+        //return 1 if we have reached last column as we are filling values col wise 
+        if(c == n) return 1;
+
+        if(r == m) return countWays(m, n, 0, c+1, 0, curr_state);
+
+        if(r == 0 && dp[c][prev_state] != -1) return dp[c][prev_state]; 
+
         int up_color = 0;
-        if (r > 0) up_color = curr_state & 3;
-        
-        int left_color = (prev_state >> ((m - r - 1) * 2)) & 3;
-        
+        if(r > 0) up_color = curr_state & 3;  //& 3 is like bitmasking last 2 digits i.e. it returns last 2 bits of a  number 
+
+        int left_color = (prev_state >> ((m-r-1) * 2)) & 3; 
+
         int ways_to_color = 0;
-        for (int color = 1; color <= 3; color++) {
-            if (color != up_color && color != left_color) {
-                ways_to_color = (ways_to_color + countWays(m, n, r + 1, c, (curr_state << 2) | color, prev_state)) % MOD;
+        for(int color = 1; color<=3; color++){
+            if(color != up_color && color != left_color){
+                ways_to_color = (ways_to_color + countWays(m, n, r+1, c, (curr_state << 2) | color, prev_state)) % MOD;
             }
         }
-        
-        if (r == 0) {
-            state_mem[c][prev_state] = ways_to_color;
+
+        if(r == 0){
+            dp[c][prev_state] = ways_to_color;
         }
+
         return ways_to_color;
     }
 }
