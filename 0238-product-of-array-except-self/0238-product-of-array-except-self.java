c2ad1261c1a039fabc078cd92eac147eeed006c4
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-25 18:31:28 +0530

    Time: 2 ms (88.93%), Space: 55.4 MB (63.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,49 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
-        //approach 1
-        /*int n = nums.length;
-        
-        int[] left = new int[n];
-        left[0] = 1;
-        
-        int[] right = new int[n];
-        right[n-1] = 1;
-        
-        //create the left product prefix array 
+        int n = nums.length;
+
+        //update left product and right product in answer array directly
+        int[] ans = new int[n];
+
+        ans[0] = 1; 
+
         for(int i=1; i<n; i++){
-            left[i] = left[i-1]*nums[i-1];   
+            ans[i] = ans[i-1] * nums[i-1];
         }
 
-        //create the right product suffix array
-        for(int j=n-2; j>=0; j--){
-            right[j] = right[j+1]*nums[j+1];   
-        }
-        
-        int[] ans = new int[n];
-        
-        //creating the ans array
-        for(int k=0; k<n; k++){
-            ans[k] = left[k]*right[k];
-        }*/
-        
-        //approach 2
-        int n = nums.length;
-        int[] ans = new int[n];
-        int curr = 1;  //we will keep on updating this value to get product
-        Arrays.fill(ans, 1);
-        
-        for(int i=0; i<n; i++){
-            ans[i] *= curr;
-            curr *= nums[i];
-        }
-        
-        curr = 1;
+        //update rp with rp variable 
+        int rp = 1;
         for(int j=n-1; j>=0; j--){
-            ans[j] *= curr;
-            curr *= nums[j];
+            ans[j] = ans[j] * rp;
+            rp = rp * nums[j]; //for next set of elements
         }
-        
+
         return ans;
     }
 }
