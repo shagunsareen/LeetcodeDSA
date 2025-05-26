702998675d04b94c1bc38b203daaf1d64d890f17
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-27 00:55:47 +0530

    Time: 6 ms (61.53%), Space: 44.8 MB (28.75%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,72 +1,49 @@
 class Solution {
+
+    //Backtracking - TLE with TC O(2^n) as if l is in set i will call wb(eetcode), if le is present i will call wb(etcode) ans so on...so i have 2 choices at each index
     /*public boolean wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        int[][] dp = new int[n][n];
-
-        for(int[] i : dp){
-            Arrays.fill(i, -1);
-        }
-        return solve(0, 0, s, new HashSet<>(wordDict), dp);
+        return canWordBreak(s, new HashSet<>(wordDict), 0);
     }
 
-    private boolean solve(int start, int end, String str, Set<String> dict, int[][] dp){
-       //base case 
-       if(dp[start][end] != -1){
-           return dp[start][end] == 1 ? true : false;
-       }
-       //base case 
-       if(end == str.length() - 1){
-           if(dict.contains(str.substring(start, end+1))){
-               return true;
-           }
-           return false;
-       }
-        //if this substring is present then we will move to next string
-       if(dict.contains(str.substring(start, end+1))){ 
-           if(solve(end + 1, end + 1, str, dict, dp)){
-               dp[start][end] = 1; // this entire string is as expected
-               return true;
-           }
-       }
-
-    //if curr substring is not present then include next index as well and check same
-       boolean ans = solve(start, end+1, str, dict, dp); 
-       dp[start][end] = ans ? 1 : 0;
-       return ans;
-    }*/
-    
-    Boolean[] present;
-    public boolean wordBreak(String s, List<String> wordDict) {
-        present = new Boolean[s.length()];
-        return checkWordBreak(s, new HashSet<>(wordDict), 0);
-    }
-    
-    private boolean checkWordBreak(String s, HashSet<String> set, int start){
-        //till where will the index go
+    private boolean canWordBreak(String s, Set<String> dict, int start){
+        //base case 
         if(start == s.length()){
             return true;
         }
-        
-        //check if string is already evaludated
-        if(present[start]!=null){
-            return present[start];
-        }
-        
-        //we will deal with substrings so end will be start + 1 else we would have taken end = start 
-        for(int end = start+1; end<=s.length() ; end++){
-            String substr = s.substring(start, end);
-            
-            //check if this is present in the hashset or not 
-            if(set.contains(substr)){
-                //check rest of the word if that is also present then return true
-                if(checkWordBreak(s, set, end)){
-                    present[start] = true;
+
+        for(int end = start + 1; end <= s.length(); end++){
+            if(dict.contains(s.substring(start, end))){ //check if l is present then check for rest of the string else create le , lee, leet and repeat same process
+                if(canWordBreak(s, dict, end))
                     return true;
-                }
             }
         }
-        
-        present[start] = false;
-        return false;
-    }
+
+        return false; //all substrings are checked with this start point and nothing is present in set then return false*/
+
+        //approach 2 - dp with memoization
+        public boolean wordBreak(String s, List<String> wordDict) {
+            return canWordBreak(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]); // here memo stores that substring starting at 'i'th index can be segmented into dict words
+        }
+
+        private boolean canWordBreak(String s, Set<String> dict, int start, Boolean[] memo){
+            if(start == s.length()){
+                return true;
+            }
+
+            if(memo[start] != null){
+                return memo[start]; //value is already calculated dont calculate again
+            }
+
+            for(int end = start + 1; end <= s.length(); end++){
+                if(dict.contains(s.substring(start, end))){ //check if l is present then check for rest of the string else create le , lee, leet and repeat same process
+                    if(canWordBreak(s, dict, end, memo)){
+                         memo[start] = true;
+                         return true;
+                    }    
+                }
+            }
+
+            memo[start] = false; // its not possible from this index 
+            return false;
+        }
 }
