10fd6abccb5f99f1e5f46c405d8a90411fbc58cf
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-26 18:43:53 +0530

    Time: 9 ms (47.81%), Space: 46.6 MB (54.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,13 +1,11 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        //sort the intervals to. know which can overlap 
+    public int[][] merge(int[][] intervals) { 
+        //sort by starting point
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
 
-        //we dont know how many elements would be there after merging hence int[][] can't be declared since size is not known, hence take list which stores merged intervals
         LinkedList<int[]> merged = new LinkedList<>();
         for(int[] interval : intervals){
-
-            if(merged.isEmpty() || merged.getLast()[1] < interval[0]){ //non overlapping 
+            if(merged.isEmpty() || merged.getLast()[1] < interval[0]){
                 merged.add(interval);
             }else{
                 merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
