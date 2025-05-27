f4b843924d03259d067c30d91473b0adf2017f3b
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-28 01:03:35 +0530

    Time: 102 ms (27.88%), Space: 105.6 MB (82.98%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.java
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.java
@@ -1,53 +1,57 @@
 class Solution {
-    private int dfs(int curr, List<List<Integer>> adj, int[] visited, int[][] longest, String colors) {
-          if (visited[curr] == 1) // Cycle found
-            return Integer.MAX_VALUE;
-        
-        if (visited[curr] == 0) { // Visiting for 1st time
-            visited[curr] = 1;
-            for (int nbr : adj.get(curr)) {
-                int res = dfs(nbr, adj, visited, longest, colors);
-                if (res == Integer.MAX_VALUE)
-                    return Integer.MAX_VALUE;
-                
-                // Iterate for each color for the current nbr and update max_len for each color at current node
-                for (int i = 0; i < 26; ++i)
-                    longest[i][curr] = Math.max(longest[i][curr], longest[i][nbr]);
-            }
-            longest[colors.charAt(curr) - 'a'][curr]++;
-            visited[curr] = 2;
-        }
-        return longest[colors.charAt(curr) - 'a'][curr];
-    }
 
     public int largestPathValue(String colors, int[][] edges) {
         int n = colors.length();
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; ++i)
+        for(int i=0; i<n; i++){ // n is the length of colors string i.e. those many nodes we will take into consideration
             adj.add(new ArrayList<>());
-        for (int[] edge : edges)
-            adj.get(edge[0]).add(edge[1]);
+        }
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]); // node -> neighbour 
+        }
 
-        int[][] longest = new int[26][n];
-        
-        //    longest: Table to store the count of each color from a given node
-        //    longest[i][j]=X: There are 'X' number of 'i' color nodes from current node 'j'
-        
-        // Perform DFS from each node and maximize color length
-        int maxColorLength = 0;
+        int[][] longest = new int[26][n]; // here value in cell will denote max count of i color nodes from current node path j 
+
+        //perform dfs from each node & maximize color length
         int[] visited = new int[n];
-        
+        int maxColorLength = 0;
+
         //    3-color method is used to detect cycle in directed graph.
         //    0: Unvisited
         //    1: Visited & Processing
         //    2: Visited & Processed
-        
-        for (int i = 0; i < n; ++i) {
+        for(int i=0; i<n; i++){
             int res = dfs(i, adj, visited, longest, colors);
-            if (res == Integer.MAX_VALUE)
+            if(res == Integer.MAX_VALUE){ //if there is a cycle in graph return -1 
                 return -1;
+            }
             maxColorLength = Math.max(maxColorLength, res);
         }
         return maxColorLength;
+    }
+
+    private int dfs(int curr, List<List<Integer>> adj, int[] visited, int[][] longest, String colors){
+        if(visited[curr] == 1){
+            return Integer.MAX_VALUE; //cycle detected
+        }
+
+        if(visited[curr] == 0){ //1st time
+            //mark it visited
+            visited[curr] = 1;
+            //go to all its adjacent nodes and update max value 
+            for(int nbr : adj.get(curr)){
+                    int res = dfs(nbr, adj, visited, longest, colors);
+                    if(res == Integer.MAX_VALUE)
+                        return Integer.MAX_VALUE;
+                    
+                    //if no cycle is returned then update max colors value for curr node for all colors with whatever path from this node to end has max values 
+                    for(int i=0; i<26; i++){
+                        longest[i][curr] = Math.max(longest[i][curr], longest[i][nbr]);
+                    }
+            }
+            longest[colors.charAt(curr) - 'a'][curr]++; //incrementing value for curr node color 
+            visited[curr] = 2;
+        }
+        return longest[colors.charAt(curr) - 'a'][curr];
     }
 }
