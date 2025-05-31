7dc1f5d761f0e859852f274fe9e31322bd043d78
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-05-31 20:56:51 +0530

    Time: 97 ms (80.71%), Space: 97.7 MB (95.44%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
@@ -1,47 +1,88 @@
 class Solution {
-
     public int[] maxTargetNodes(int[][] edges1, int[][] edges2) {
-        int n = edges1.length + 1, m = edges2.length + 1;
-        int[] color1 = new int[n];
-        int[] color2 = new int[m];
-        int[] count1 = build(edges1, color1);
-        int[] count2 = build(edges2, color2);
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = count1[color1[i]] + Math.max(count2[0], count2[1]);
-        }
-        return res;
-    }
+        int n1 = edges1.length + 1;
+        int n2 = edges2.length + 1;
 
-    private int[] build(int[][] edges, int[] color) {
-        int n = edges.length + 1;
-        List<List<Integer>> children = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            children.add(new ArrayList<>());
-        }
-        for (int[] edge : edges) {
-            children.get(edge[0]).add(edge[1]);
-            children.get(edge[1]).add(edge[0]);
-        }
-        int res = dfs(0, -1, 0, children, color);
-        return new int[] { res, n - res };
-    }
+        List<List<Integer>> neigh1 = build(edges1, n1);
+        List<List<Integer>> neigh2 = build(edges2, n2);
 
-    private int dfs(
-        int node,
-        int parent,
-        int depth,
-        List<List<Integer>> children,
-        int[] color
-    ) {
-        int res = 1 - (depth % 2);
-        color[node] = depth % 2;
-        for (int child : children.get(node)) {
-            if (child == parent) {
-                continue;
+        int[] count2 = bfs(0, neigh2, null);
+        int evenCount2 = count2[0];
+        int oddCount2 = count2[1];
+        int best2 = Math.max(evenCount2, oddCount2);
+       
+        boolean[] isEvenLevel = new boolean[n1];
+        int[] count1 = bfs(0, neigh1, isEvenLevel);
+        int evenCount1 = count1[0];
+        int oddCount1 = count1[1];
+        
+        int[] res = new int[n1];
+
+        for(int i=0; i<n1; i++){
+            if(isEvenLevel[i]){
+                res[i] = evenCount1 + best2;   
+            }else{
+                res[i] = oddCount1 + best2;
             }
-            res += dfs(child, node, depth + 1, children, color);
         }
+
         return res;
+    }
+
+    private List<List<Integer>> build(int[][] edges, int n){
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        
+        for(int i=0; i<n; i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges){
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+
+        return adjList;
+    }
+
+    //return even nodes from starting node 
+    private int[] bfs(int startNode, List<List<Integer>> neighbours, boolean[] isEvenLevel){
+        int n = neighbours.size(); //node count int tree
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        queue.offer(startNode);
+        visited[startNode] = true;
+
+        int level = 0;
+        int even = 0;
+        int odd = 0;
+    
+        while(!queue.isEmpty()){ 
+            int size = queue.size();
+
+            for(int i=0; i<size; i++){
+                int curr = queue.poll();
+
+                if(level % 2 == 0){
+                    even++;
+                    if(isEvenLevel != null){
+                        isEvenLevel[curr] = true;
+                    }    
+                }else{
+                    odd++;
+                }
+
+                for(int neigh : neighbours.get(curr)){
+                    if(!visited[neigh]){
+                        visited[neigh] = true;
+                        queue.offer(neigh);
+                    }
+                }
+            }
+
+            level++;
+        }
+        return new int[]{even, odd};
     }
 }
