38cd60ad75c3595b03c9a14580b7daa3337d3399
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-07-16 01:05:12 +0530

    Time: 1 ms (99.14%), Space: 42.3 MB (35.12%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3){
+            return false;
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonent = false;
+        for(char c : word.toCharArray()){
+            if(Character.isLetter(c)){
+                char ch = Character.toLowerCase(c);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    hasVowel = true;
+                }else{
+                    hasConsonent = true;
+                }
+            }else if(!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return hasVowel && hasConsonent;
+    }
+}
