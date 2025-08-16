383bcab5bbc33812270def00dd5a265a21505143
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-08-16 16:50:31 +0530

    Time: 1 ms (99.54%), Space: 57.2 MB (42.57%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,18 +1,23 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
         int n = nums.length;
 
-        for(int i=0; i<n; i++){
-            sum += nums[i];
-            maxSum = Math.max(maxSum, sum);
+        if(n == 1) return nums[0];
+
+        //kadane's algo
+        int maxSum = Integer.MIN_VALUE; //can't take 0 as if all elements are negative then 0 would be maxsum which is not true
+
+        int sum = 0;
+
+        for(int num : nums){
+            sum += num;
+
+            maxSum = Math.max(sum, maxSum);
 
             if(sum < 0){
-                sum = 0;
-            }  
-        }       
-
+                sum = 0; //reset
+            }
+        }
         return maxSum;
     }
 }
