faefc24d0225e8d0a6ba859d46bcba15f88ba54b
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-08-16 23:13:49 +0530

    Time: 18 ms (23.04%), Space: 49.4 MB (5.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        
-        //Approach 1 : TC : O(n*klogk)
-        //Sort string and store it as key of map, then store all strings whose key is same in the map 
-        //iterate over all strings and collect strings with same sorted key
-        /*for(String str : strs){
-            char[] ch = str.toCharArray();
-            Arrays.sort(ch);
-            String sortedStr = String.valueOf(ch);
-        
-            map.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
-        }*/
-        
-        
-        //Approach 2 : Skip Sorting and Create your own string 
+
         for(String str : strs){
-            //store freq of each ch and create a string out of that
-            char[] ch = new char[26];
-            for(char c : str.toCharArray()){
-                ch[c-'a']++; //increasing freq of current character
+
+            int[] freq = new int[26];
+            for(char ch : str.toCharArray()){
+                freq[ch - 'a']++;
             }
-            String keyStr = String.valueOf(ch);
-            map.computeIfAbsent(keyStr, k -> new ArrayList<>()).add(str);
+
+            //convert this array to string
+            String key = Arrays.toString(freq);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
         }
+
         return new ArrayList<>(map.values());
     }
 }
