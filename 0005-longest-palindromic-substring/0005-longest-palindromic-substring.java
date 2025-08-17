058951ec22dfc3f33899214e2bdf8ff4cd9c6b9c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-08-17 13:33:02 +0530

    Time: 19 ms (51.52%), Space: 45.5 MB (26.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,38 +2,33 @@ class Solution {
     public String longestPalindrome(String s) {
         if(s.length() <= 1)
             return s;
-        
-        //Expand outward center approach 
-        //Consider each character as the center of the subtring and expand outwards if the characters match 
-        //For every char we will check oddPlindrome and evenPalindrome 
+
         String maxStr = s.substring(0,1);
-        
+
+        //expand outwards from center approach
+        //consider odd and even length strings and check if palindrome
         for(int i=0; i<s.length(); i++){
-            
-            //consider the current element only 
-            String oddStr = expandAroundCenter(s, i, i);  //this will give the length of the palindromic string
-            
-            //consider current element and neighbouring element
-            String evenStr = expandAroundCenter(s, i, i+1);
-            
-            if(oddStr.length() > maxStr.length())
+
+            String oddStr = expand(s, i, i); 
+            String evenStr = expand(s, i, i+1);
+
+            if(oddStr.length() > maxStr.length()){
                 maxStr = oddStr;
-            
-            if(evenStr.length() > maxStr.length())
+            }
+
+            if(evenStr.length() > maxStr.length()){
                 maxStr = evenStr;
+            }
         }
-        
+
         return maxStr;
     }
-    
-    
-    private String expandAroundCenter(String str, int left, int right){
-        
-        while(left >= 0 && right <= str.length()-1 && str.charAt(left) == str.charAt(right)){
+
+    private String expand(String str, int left, int right){
+        while(left >=0 && right < str.length() && str.charAt(left) == str.charAt(right)){
             left--;
-            right++; //if characters match lets explore more on both ends
-        } 
-        return str.substring(left+1, right);  
-        //since left and right will be incremented and will go out of bounds hence increment left and decrement right will be done automatically by subtring function as it considers second param index -1;
+            right++;
+        }
+        return str.substring(left+1, right); //since we need to include only 
     }
 }
