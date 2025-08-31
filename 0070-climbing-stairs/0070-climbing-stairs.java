a71e728eec60c29dfac54f1fb8cd72143ba99c57
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-08-31 18:04:14 +0530

    Time: 0 ms (100%), Space: 40.2 MB (82.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,52 @@
 class Solution {
-    public int climbStairs(int n) {
-        // either we can take one step or two steps that means if i want to get no of ways to climb at n then to reach n i can either come from n-1th step or n-2th step i.e. f(n) = f(n-1) + f(n-2)
-        //It is similar to fibonacci series 
 
-        //Tabulation - bottom up approach 
+    //Recursion  - TC : O(2^n), SC : O(n)
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        dp[0] = 1;
-        dp[1] = 1;
+    /*public int climbStairs(int n) {
+        return climbStairs(0, n);
+    }
 
-        if(n <= 1){
-            return dp[n];
+    private int climbStairs(int index, int n){
+        if(index > n){
+            return 0;
         }
 
-        for(int i=2; i<=n; i++){
+        if(index == n){
+            return 1;
+        }
+
+        return climbStairs(index + 1, n) + climbStairs(index + 2, n); 
+        //at every point we have 2 paths to take so whatever is current state we take both paths one by one and see if we reach destination
+    }*/
+
+    //Memoization - keep array to store pre computed solutions (Top Down) is with recursion, TC:O(n) SC:O(n)
+    /*public int climbStairs(int n) {
+        int[] memo = new int[n+1]; //since we need result for 3 so if we get climbStairs(3) and if its already calculated before then we can directly return that value so we take n+1
+        return climbStairs(0, n, memo);
+    }
+
+    private int climbStairs(int index, int n, int[] memo){
+        if(index > n){
+            return 0;
+        }
+
+        if(index == n){
+            return 1;
+        }
+
+        memo[index] = climbStairs(index + 1, n, memo) + climbStairs(index + 2, n, memo);
+        return memo[index];
+    }*/
+
+    //Tabulation - Bottom up - this is iterative , TC:O(n) SC:O(n)
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
-       
-        return dp[n];  
+        return dp[n];
     }
 }
