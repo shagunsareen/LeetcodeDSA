199ad300e3b77faf2e449e264f4a087411a09c0d
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-04 09:46:06 +0530

    Time: 3 ms (4.98%), Space: 45 MB (5.1%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
 
     //Approach 1 - Tabulation , Iterative, Bottom to Up
-    public int minCostClimbingStairs(int[] cost) {
+    /*public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
 
         int minCost[] = new int[n + 1]; 
@@ -15,5 +15,29 @@ class Solution {
         }
 
         return minCost[minCost.length -1];
+    }*/
+
+    //Approach 2 - Top Down , Recursive, Memoization
+    Map<Integer, Integer> map = new HashMap<>();
+    public int minCostClimbingStairs(int[] cost) {   
+      int n = cost.length;
+      return minCost(n, cost); //we start from top value and store values calculated on the way to reach bottom indexes 
+    }
+
+    private int minCost(int step, int[] cost){
+        if(step <= 1){
+            return 0; //since we can start from both and mincost for starting point will be 0
+        }
+
+        if(map.containsKey(step)){
+            return map.get(step);
+        }
+
+        int oneStepCost = minCost(step - 1, cost) + cost[step-1];
+        int twoStepCost = minCost(step - 2, cost) + cost[step-2];
+        int value = Math.min(oneStepCost, twoStepCost);
+        map.put(step, value);
+
+        return value;
     }
 }
