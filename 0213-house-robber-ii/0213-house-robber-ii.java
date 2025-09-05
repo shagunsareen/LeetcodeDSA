4780818acff92362934b3d471a661c92adb9c956
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-05 09:58:22 +0530

    Time: 0 ms (100%), Space: 41.6 MB (5.64%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -35,7 +35,7 @@ class Solution {
         dp[index] = Math.max(pick, dontPick);
 
         return dp[index];
-    }*/
+    }
 
     //Tabulation
     int[] dp;
@@ -69,6 +69,38 @@ class Solution {
             dp[i] = Math.max(pick, dontPick);
         }
         return dp[0];
+    }*/
+
+    //Space optimised
+    public int rob(int[] nums) {
+        //split array into 2 arrs, one with last element and one without last element
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        int[] first = Arrays.copyOfRange(nums, 0, n-1);
+        int[] second = Arrays.copyOfRange(nums, 1, n); 
+
+        int firstCost  = robFrom(first);
+        int secondCost = robFrom(second);
+
+        return Math.max(firstCost, secondCost);
+    }
+
+    private int robFrom(int[] nums){
+        int n = nums.length;
+
+        int next = nums[n-1];
+        int nextAdjacent = 0;
+
+        for(int i=n-2; i>=0; i--){
+            int pick = nextAdjacent + nums[i];
+            int dontPick = next;
+
+            int currMax = Math.max(pick, dontPick);
+            nextAdjacent = next;
+            next = currMax;
+        }
+        return next;
     }
 }
 
