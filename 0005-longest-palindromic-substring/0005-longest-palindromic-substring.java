baa11ce59b75b47ad9ab98898d1bf64945c7dbc7
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-05 21:25:13 +0530

    Time: 124 ms (30.87%), Space: 46.5 MB (11.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,5 +1,7 @@
 class Solution {
-    public String longestPalindrome(String s) {
+
+    //Expand around centres approach TC : o(n2), SC : o(1)
+    /*public String longestPalindrome(String s) {
         int n = s.length();
 
         if(n == 1){
@@ -35,5 +37,38 @@ class Solution {
         }
 
         return s.substring(left + 1, right);
+    }*/
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] ans = new int[]{0,0};
+
+        //all 1 length substrings are palindrome 
+        for(int i=0; i<n; i++){
+            dp[i][i] = true;
+        }
+
+        //two length substrings
+        for(int i = 0; i < n-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                dp[i][i+1] = true;
+                ans[0] = i;
+                ans[1] = i+1;
+            }
+        }
+
+        for(int diff = 2; diff < n; diff++){
+            for(int i = 0; i < n - diff; i++){
+                int j = i + diff;
+                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+
+        return s.substring(ans[0], ans[1]+1);
     }
 }
