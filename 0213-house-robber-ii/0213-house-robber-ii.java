a9b3b736112452d32d2c2a226b6aeca3bd33d9bd
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-05 09:51:31 +0530

    Time: 0 ms (100%), Space: 41.1 MB (49.49%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,5 +1,7 @@
 class Solution {
-    int[] dp;
+    
+    //Memoization 
+    /*int[] dp;
     public int rob(int[] nums) {
         //split array into 2 arrs, one with last element and one without last element
         int n = nums.length;
@@ -33,52 +35,40 @@ class Solution {
         dp[index] = Math.max(pick, dontPick);
 
         return dp[index];
+    }*/
+
+    //Tabulation
+    int[] dp;
+    public int rob(int[] nums) {
+        //split array into 2 arrs, one with last element and one without last element
+        int n = nums.length;
+
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        if(n == 1) return nums[0];
+
+        int[] first = Arrays.copyOfRange(nums, 0, n-1);
+        int[] second = Arrays.copyOfRange(nums, 1, n); 
+
+        int firstCost  = robFrom(first);
+        Arrays.fill(dp, -1);
+        int secondCost = robFrom(second);
+
+        return Math.max(firstCost, secondCost);
+    }
+
+    private int robFrom(int[] nums){
+        int n = nums.length;
+        dp[n-1] = nums[n-1];
+        dp[n] = 0;
+        for(int i=n-2; i>=0; i--){
+            int pick = dp[i+2] + nums[i];
+            int dontPick = dp[i+1];
+
+            dp[i] = Math.max(pick, dontPick);
+        }
+        return dp[0];
     }
 }
 
-
-/*class Solution {
-    int[] dp;
-
-    public int rob(int[] nums) {
-        int n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-
-        if(n==1) return nums[0];
-
-        //create two separate lists one without first element and second without last element
-        int[] first = new int[n];
-        int[] second = new int[n];
-
-        for(int i=0; i<n; i++){
-            if(i!=0) second[i] = nums[i];
-            if(i!=n-1) first[i] = nums[i];
-        }
-
-        int firstArrCost = getMaxRobAmount(n-1, first);
-
-        Arrays.fill(dp, -1);
-
-        int secondArrCost = getMaxRobAmount(n-1, second);
-
-        return Math.max(firstArrCost, secondArrCost);
-    }
-
-    private int getMaxRobAmount(int index, int[] nums){
-        if(index == 0) return nums[index];
-
-        if(index < 0) return 0;
-
-        if(dp[index] != -1) return dp[index];
-
-        //either we pick an element or we don't
-        int pick = nums[index]  + getMaxRobAmount(index - 2, nums);
-
-        int notPick = getMaxRobAmount(index - 1, nums);
-
-        dp[index] = Math.max(pick, notPick);
-
-        return dp[index];
-    }
-}*/
