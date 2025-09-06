cd4cec6cbf83fb833dec979b6cf068a80cb5281e
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-06 22:53:27 +0530

    Time: 1 ms (82.43%), Space: 42.1 MB (37.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -34,7 +34,7 @@ class Solution {
         memo.put(index, ans);
 
         return ans;
-    }*/
+    } 
 
     //Iterative Approach 
     public int numDecodings(String s) {
@@ -59,5 +59,35 @@ class Solution {
         }  
         return dp[s.length()];      
     }
-    
+    */
+
+    //Space Optimised Iterative Approach 
+    public int numDecodings(String s) {
+        int n = s.length();
+        
+        int prev = 1;
+
+        //ways to decode string of size 1
+        int curr = s.charAt(0) == '0' ? 0 : 1;
+        
+        for(int i=2; i<=s.length(); i++){
+            int temp = 0;
+
+            //check for single digit decode 
+             if(s.charAt(i-1) != '0'){
+                 temp = curr;
+             }   
+
+             //check if successful two digit decode is possible 
+             int twoDigit = Integer.valueOf(s.substring(i-2, i));
+             if(twoDigit >= 10 && twoDigit <= 26){
+                temp += prev;
+             }
+
+            prev = curr;
+            curr = temp;
+        } 
+         
+        return curr;      
+    }
 }
