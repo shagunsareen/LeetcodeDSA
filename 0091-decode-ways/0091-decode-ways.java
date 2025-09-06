0599b451d016c2d2a9521f58dd75aac6846daf32
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-06 21:15:46 +0530

    Time: 1 ms (82.43%), Space: 42.2 MB (25.28%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,36 @@
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+    
+    public int numDecodings(String s) {
+        return decode(0, s);
+    }
+
+    private int decode(int index, String s){
+        if(memo.containsKey(index)){
+            return memo.get(index);
+        }
+
+        if(index == s.length()){
+            return 1;
+        }
+
+        if(s.charAt(index) == '0'){
+            return 0; //cant decode string starting with 0
+        }
+
+        if (index == s.length() - 1) {
+            return 1;
+        }
+
+        int ans = decode(index + 1, s);
+
+        if(Integer.parseInt(s.substring(index, index + 2)) <= 26){
+            ans += decode(index + 2, s);
+        }
+
+        //save for memoization that at this index what all 
+        memo.put(index, ans);
+
+        return ans;
+    }
+}
