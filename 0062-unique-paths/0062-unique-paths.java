c06d7f0f228cf5be7724fcc9f2c338ce2b5193f3
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-07 19:30:36 +0530

    Time: 0 ms (100%), Space: 40.2 MB (89.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,131 +1,83 @@
 class Solution {
-    /*
-    Approach - Recursion - TLE as TC is 2^(m+n)
-    public int uniquePaths(int m, int n) {
-       return getUniquePaths(m, n, 0, 0);
-    }
 
-    private int getUniquePaths(int m, int n, int row, int col){
-        //out of bound indexes
-        if(row >= m || col >= n){
-            return 0;
-        }
-
-        //base case - reached last cell
-        if(row == m-1 && col == n-1){
-            return 1;
-        }
-
-        int bottom = getUniquePaths(m, n, row+1, col);
-        int right = getUniquePaths(m, n, row, col + 1);
-
-        return bottom + right;
-    }
-    */
-
-    //Approach 2 - Memoization - TC : O(mxn) , total of m × n unique states (each cell in the grid is a state) & each state is being computed once 
+    //memoization
     /*int[][] dp;
     public int uniquePaths(int m, int n) {
-       dp = new int[m][n]; 
-
-       for(int[] row : dp){
-         Arrays.fill(row, -1);
-       }
-       
-       return getUniquePaths(m, n, 0, 0);
+        dp = new int[m][n];
+        for(int[] row : dp){
+             Arrays.fill(row, -1);
+        }
+        return uniquePathWays(m , n , m-1, n-1); //start from (m-1, n-1) to (0,0)
     }
 
-    private int getUniquePaths(int m, int n, int row, int col){
-        //out of bound indexes
-        if(row >= m || col >= n){
-            return 0;
+    private int uniquePathWays(int m , int n, int row, int col){
+        if(row == 0 && col == 0){ 
+            return 1;
         }
 
-        //base case - reached last cell
-        if(row == m-1 && col == n-1){
-            return 1;
+        if(row < 0 || col < 0)
+        {
+            return 0;
         }
 
         if(dp[row][col] != -1){
             return dp[row][col];
         }
 
-        int bottom = getUniquePaths(m, n, row+1, col);
-        int right = getUniquePaths(m, n, row, col + 1);
+        int up = uniquePathWays(m , n, row-1, col);
 
-        dp[row][col] = bottom + right;
+        int left = uniquePathWays(m , n, row, col - 1);
+
+        dp[row][col] = up + left;
+        
         return dp[row][col];
+    }
+
+
+    //Tabulation
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        
+        Arrays.fill(dp[0], 1);
+
+        for(int row = 1; row < m; row++){
+            for(int col = 0; col < n; col++){
+                
+                if(row > 0){
+                    dp[row][col] += dp[row-1][col];
+                }
+                
+                if(col > 0){
+                    dp[row][col] += dp[row][col-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }*/
 
-    //Approach 3 : Tabulation 
-    /*int[][] dp;
+    //Space optimisation
     public int uniquePaths(int m, int n) {
-       dp = new int[m][n]; 
+        int[] prev = new int[n];      
+        
+        Arrays.fill(prev, 1);
 
-       for(int[] row : dp){
-         Arrays.fill(row, -1);
-       }
-       
-       for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-
-            //if we have reached last cell
-            if(i == 0 && j == 0){
-                dp[i][j] = 1;
-                continue;
-            }
-
-            //either we move up or we move left
-            int up = 0;
-            int left = 0;
-
-            if(j > 0){
-                up = dp[i][j-1];
-            }
+        for(int row = 1; row < m; row++){
             
-            if(i > 0){
-                left = dp[i-1][j];
-            }
-            
-            dp[i][j] = up + left;
-        }
-       }
+            int[] curr = new int[n];
 
-       return dp[m-1][n-1]; //return total number of ways to reach last cell */
-
-       //Approach 4: space optimisation 
-       public int uniquePaths(int m, int n) {
-
-            //we at any time need upper row value and prev cell values as we can go either up or left
-            //so for upper row we will maintain prev[] and for curr row left values we put those in temp[]
-
-            int[] prev = new int[n];
-
-            for(int i=0; i<m; i++){
-                //for every curr row store values in temp arr
-                int[] temp = new int[n];
-                for(int j=0; j<n; j++){
-                    
-                    if(i == 0 && j == 0){
-                        temp[j] = 1;
-                        continue;
-                    }
-
-                    int up = 0;
-                    int left = 0;
-
-                    if(i > 0){
-                        up = prev[j]; //prev row data 
-                    }
-
-                    if(j > 0){
-                        left = temp[j-1]; //current row data
-                    }
-
-                    temp[j] = up + left;
+            for(int col = 0; col < n; col++){
+                
+                if(row > 0){
+                    curr[col] += prev[col];
                 }
-                prev = temp;
+                
+                if(col > 0){
+                    curr[col] += curr[col-1];
+                }
             }
-            return prev[n-1]; 
-       }
+
+            prev = curr;
+        }
+        return prev[n-1];
+    }
 }
