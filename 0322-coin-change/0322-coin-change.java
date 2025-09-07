9ebc9d62904d82036f2f900950ca7b0c971f5188
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-07 16:24:49 +0530

    Time: 18 ms (41.31%), Space: 45.4 MB (18.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,18 +4,16 @@ class Solution {
     /*int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        dp = new int[n][amount+1];
-
+        dp = new int[n][amount + 1]; //at certain index all elements from 0 to ith index can give Target in how many coins
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
 
-        int result = getMinCoins(coins, amount, n - 1);
-        return result == Integer.MAX_VALUE ? -1 : result;
+        int res = getMinCoin(n-1, coins, amount) ;
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    private int getMinCoins(int[] coins, int amount, int index){
-        //base case 
+    private int getMinCoin(int index, int[] coins, int amount){
         if(amount == 0){
             return 0;
         }
@@ -31,47 +29,48 @@ class Solution {
             return dp[index][amount];
         }
 
-        int taken = Integer.MAX_VALUE;
+        int take = Integer.MAX_VALUE;
 
         if(coins[index] <= amount){
-            int res = getMinCoins(coins, amount - coins[index], index);
-            if(res != Integer.MAX_VALUE){
-                taken = 1 + res;
-            }       
+            int res = getMinCoin(index, coins, amount - coins[index]); 
+            // since we can take same coin multiple times we dont decrement index
+            if(res != Integer.MAX_VALUE)
+            {
+                take = 1 + res;
+            }
         }
 
-        int notTaken = getMinCoins(coins, amount, index-1);
+        int notTake = getMinCoin(index - 1, coins, amount);
 
-        dp[index][amount] = Math.min(taken, notTaken);
+        dp[index][amount] = Math.min(notTake, take);
         return dp[index][amount];
     }*/
-
+    
     //Tabulation 
     /*int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        dp = new int[n][amount+1];
+        dp = new int[n][amount + 1];
 
         //first row 
-        for(int t=0; t<=amount; t++){
+        for(int t=0; t<=amount; t++){               
             if(t % coins[0] == 0){
-                dp[0][t] = t / coins[0];
+                dp[0][t] = t/coins[0];
             }else{
                 dp[0][t] = Integer.MAX_VALUE;
             }
         }
-        
-        //fill rest of the table
-        for(int index = 1; index < n; index++){
-            for(int target=0; target <= amount; target++){
-                
+
+        //fill rest of the table 
+        for(int index = 1; index <n; index++){
+            for(int target = 0; target <= amount; target++){
                 int taken = Integer.MAX_VALUE;
 
                 if(coins[index] <= target){
                     int res = dp[index][target - coins[index]];
                     if(res != Integer.MAX_VALUE){
                         taken = 1 + res;
-                    }       
+                    }
                 }
 
                 int notTaken = dp[index-1][target];
@@ -79,16 +78,17 @@ class Solution {
                 dp[index][target] = Math.min(taken, notTaken);
             }
         }
-        
+
         return dp[n-1][amount] == Integer.MAX_VALUE ? -1 : dp[n-1][amount];
     }*/
 
-    //Space optimization
-    int[] prev;
+    //Space optimised 
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        prev = new int[amount + 1]; 
+        int n =coins.length;
+        int[] prev = new int[amount + 1];
+        
 
+        //fill prev row so that we have prev for next all rows
         for(int t=0; t<=amount; t++){
             if(t % coins[0] == 0){
                 prev[t] = t / coins[0];
@@ -97,31 +97,28 @@ class Solution {
             }
         }
 
+        //for other indexes
         for(int index = 1; index < n; index++){
-            
+
             int[] curr = new int[amount + 1];
 
-            for(int target=0; target <= amount; target++){
+            for(int target = 0; target <= amount; target++){
                 
                 int taken = Integer.MAX_VALUE;
-
+                
                 if(coins[index] <= target){
-
                     int res = curr[target - coins[index]];
-                    
                     if(res != Integer.MAX_VALUE){
                         taken = 1 + res;
-                    }       
+                    }
                 }
 
                 int notTaken = prev[target];
 
                 curr[target] = Math.min(taken, notTaken);
-            }
+            }   
             prev = curr;
-        }      
+        }
         return prev[amount] == Integer.MAX_VALUE ? -1 : prev[amount];
     }
-
-
 }
