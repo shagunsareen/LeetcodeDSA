a178c108099d1eda28db1e07540005b618634619
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-08 19:55:26 +0530

    Time: 6 ms (22.1%), Space: 46.1 MB (6.19%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    //Approach : Memoization
+    //Memoization - since we do recursion from 1 point hence starting from top instead of bottom
     /*int[][] dp;
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
@@ -10,83 +10,47 @@ class Solution {
             Arrays.fill(row, -1);
         }
 
-        return getMinSum(triangle, 0, 0); 
+        return getPathSum(triangle, 0, 0, n);
     }
 
-    //gives sum till the current co-ordinates
-    private int getMinSum(List<List<Integer>> triangle, int row, int col){
-        int n = triangle.size();
-
-        if(dp[row][col] != -1){
-            return dp[row][col];
-        }
-
+    private int getPathSum(List<List<Integer>> triangle, int row, int col, int n){
+        //base case - reached last row
         if(row == n-1){
             return triangle.get(row).get(col);
         }
 
-        int curr = triangle.get(row).get(col);
+        if(dp[row][col] != -1)
+        {
+            return dp[row][col];
+        }
 
-        int down = curr + getMinSum(triangle, row + 1, col);
-        int diag = curr + getMinSum(triangle, row + 1, col + 1);
+        int down = triangle.get(row).get(col) + getPathSum(triangle, row + 1, col, n);
 
-        dp[row][col] = Math.min(down, diag);
+        int diagonal = triangle.get(row).get(col) + getPathSum(triangle, row + 1, col + 1, n);
 
+        dp[row][col] = Math.min(down, diagonal);
         return dp[row][col];
     }*/
 
-    //Approach 2 : Tabulation - Bottom up approach
-    /*int[][] dp;
-
+    //Tabulation
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        dp = new int[n][n];
+        int[][] dp = new int[n][n];
 
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-
-        for(int col = 0; col <n; col++){
+        //fill dp table from below
+        for(int col = 0; col<n; col++){
             dp[n-1][col] = triangle.get(n-1).get(col);
         }
 
-        for(int i=n-2; i>=0; i--){
-            for(int j=i; j>=0; j--){
-                
-                int down = triangle.get(i).get(j) + dp[i+1][j];
-                int diag = triangle.get(i).get(j) + dp[i+1][j+1];
+        for(int row = n-2; row >= 0; row--){
+            for(int col = row; col >= 0; col--){
+               int down = triangle.get(row).get(col) + dp[row + 1][col];
+               int diagonal = triangle.get(row).get(col) + dp[row + 1][col + 1];
 
-                dp[i][j] = Math.min(down, diag);
+               dp[row][col] = Math.min(down, diagonal);
             }
         }
-
         return dp[0][0];
-    }*/
-
-    //Approach 3 : space optimisation
-    int[] front;
-    int[] curr;
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        front = new int[n];
-
-        for(int col = 0; col <n; col++){
-            front[col] = triangle.get(n-1).get(col);
-        }
-
-        for(int i=n-2; i>=0; i--){
-            curr = new int[n];
-
-            for(int j=i; j>=0; j--){
-                
-                int down = triangle.get(i).get(j) + front[j];
-                int diag = triangle.get(i).get(j) + front[j+1];
-
-                curr[j] = Math.min(down, diag);
-            }
-            front = curr;
-        }
-
-        return front[0];
     }
+
 }
