aee1d4c44a826025887b40d4b1c1151218d03f53
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-14 16:43:59 +0530

    Time: 3 ms (93.77%), Space: 42.9 MB (58.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,51 +1,47 @@
 class Solution {
+
+    //Memoization
     public int findTargetSumWays(int[] nums, int target) {
-        //get the totalSum 
-        int n = nums.length;
         int totalSum = 0;
+
         for(int num : nums){
             totalSum += num;
         }
 
-        if (totalSum - target < 0 || (totalSum - target) % 2 == 1)
-            return 0;
+        if(totalSum - target < 0 || (totalSum - target) % 2 == 1) return 0;
 
-        //we have to find subsets with (totalSum - target)/2 diff 
-        int s2Sum = (totalSum - target)/2;
-        int s1Sum = totalSum - s2Sum;
+        int targetSum = (totalSum - target)/2;
+        int n = nums.length;
+        int[][] dp = new int[n][targetSum + 1];
 
-        return countPartitions(nums, s2Sum);
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
+        }
+
+        return memoization(nums, targetSum, n-1, dp);
     }
 
-    int[][] dp;
-    private int countPartitions(int[] nums, int sum){
-        int n = nums.length;
-        dp = new int[n][sum + 1]; // n - index , target + 1 - sum at each col
-
-        //0th row 
-        if(nums[0] == 0){
-            dp[0][0] = 2; //either take 0 or not sum is achieved
-        }else{
-            dp[0][0] = 1; //not pick 
-            if(nums[0] <= sum){ //meaning we can pick element at 0th index since its value is less than the target
-                dp[0][nums[0]] = 1;
+    private int memoization(int[] nums, int target, int index, int[][] dp){
+        if(index == 0){
+            if(target == 0 && nums[0] == 0){
+                return 2;
+            }else if( target == 0 || nums[0] == target){
+                return 1; 
             }
+            return 0;
+        } 
+
+        if(dp[index][target] != -1){
+                return dp[index][target];
         }
 
-        for(int index = 1; index < n; index++){
-            for(int target = 0; target <= sum; target++){
-
-                int notPick = dp[index-1][target];
-
-                int pick = 0;
-
-                if(nums[index] <= target){
-                    pick = dp[index-1][target - nums[index]];
-                }
-
-                dp[index][target] = pick + notPick;
-            }
+        int take = 0;
+        if(nums[index] <= target){
+            take = memoization(nums, target - nums[index], index - 1, dp);
         }
-        return dp[n-1][sum];
+        int notTake = memoization(nums, target, index - 1, dp);
+        dp[index][target] = take + notTake;
+        
+        return dp[index][target];
     }
 }
