b86f4d141c8445d19643c1e4f795b49bf778869a
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-14 17:29:02 +0530

    Time: 5 ms (83.32%), Space: 43.6 MB (46.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -14,11 +14,13 @@ class Solution {
         int n = nums.length;
         int[][] dp = new int[n][targetSum + 1];
 
-        for(int[] rows : dp){
+        /*for(int[] rows : dp){
             Arrays.fill(rows, -1);
         }
 
-        return memoization(nums, targetSum, n-1, dp);
+        return memoization(nums, targetSum, n-1, dp); */
+
+        return tabulation(nums, targetSum, dp);
     }
 
     private int memoization(int[] nums, int target, int index, int[][] dp){
@@ -43,5 +45,32 @@ class Solution {
         dp[index][target] = take + notTake;
         
         return dp[index][target];
+    }
+
+    public int tabulation(int[] nums, int target, int[][] dp) {
+        int n = nums.length;
+
+        if(nums[0] == 0){
+            dp[0][0] = 2;
+        }else{
+            dp[0][0] = 1; //if there is any other value on 0th index but sum we are expecting is 0 then we ignore it
+        }
+
+        if(nums[0] != 0 && nums[0] <= target){ // if value at 0th index is anything other than 0 and can be included to target sum then we will pickit
+            dp[0][nums[0]] = 1; 
+        }
+
+        for(int index = 1; index < n; index++){
+            for(int tar = 0; tar <= target; tar++){
+                int take = 0;
+                if(nums[index] <= tar){
+                    take = dp[index - 1][tar - nums[index]];
+                }
+                int notTake = dp[index - 1][tar];
+                dp[index][tar] = take + notTake;
+            }
+        }
+
+        return dp[n-1][target];
     }
 }
