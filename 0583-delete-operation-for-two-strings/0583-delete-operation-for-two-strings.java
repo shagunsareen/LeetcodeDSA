cf1e84f64e572f0ed7ea16408b9cda2d63cc7278
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-21 19:51:15 +0530

    Time: 7 ms (69.2%), Space: 44.6 MB (86.5%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        return n1 + n2 - 2 * getLongestCommonSubsequence(word1, word2, n1, n2);
+    }
+
+    //tabulation
+    private int getLongestCommonSubsequence(String word1, String word2, int n1, int n2){
+        
+        int[][] dp = new int[n1 + 1][n2 + 1];
+
+        //fill 0 length string as 0 since it cant have subsequence
+        for(int row = 0; row <= n1; row++){
+            dp[row][0] = 0;
+        }
+
+        for(int col = 0; col <= n2; col++){
+            dp[0][col] = 0;
+        }
+
+        for(int row = 1; row <= n1; row++){
+            for(int col = 1; col <= n2; col++){
+                if(word1.charAt(row - 1) == word2.charAt(col - 1)){
+                    dp[row][col] = 1 + dp[row - 1][col - 1];
+                }else{
+                    dp[row][col] = Math.max(dp[row - 1][col], dp[row][col - 1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }   
+}
