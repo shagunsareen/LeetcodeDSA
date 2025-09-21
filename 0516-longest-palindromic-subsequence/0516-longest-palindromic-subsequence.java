d1d2c17cfa1a49308428d80461ebf81e8a7628ad
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-21 16:23:00 +0530

    Time: 55 ms (45.92%), Space: 55.2 MB (72.36%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    //Tabulation
+    public int longestPalindromeSubseq(String s) {   
+        String reverseS = new StringBuilder(s).reverse().toString();
+        return checkLongestPalindromeSubseq(s, reverseS);
+        //if a subsequence is in s1 and is in its reverse string also then that is common subsequence but that will automatically be palindorme as it has same characters in reverse string 
+    }
+
+    private int checkLongestPalindromeSubseq(String s1, String s2){
+        int len1 = s1.length();
+        int len2 = s1.length();
+
+        int[][] dp = new int[len1+1][len2+1]; //we shift 1 index in tabulation 
+
+        //for 0 length string there wont be any common stringhence fill it with 0
+        for(int row = 0; row <= len1; row++)
+        {
+            dp[row][0] = 0;
+        }
+
+        for(int col = 0; col <= len2; col++){
+            dp[0][col] = 0;
+        }
+
+        for(int row = 1; row <= len1; row++){
+            for(int col = 1; col <= len2; col++){
+                if(s1.charAt(row - 1) == s2.charAt(col - 1)){
+                    dp[row][col] = 1 + dp[row - 1][col - 1];
+                }else{
+                    dp[row][col] = Math.max(dp[row][col - 1], dp[row - 1][col]);
+                }
+            }
+        }
+
+        return dp[len1][len2];
+    }
+}
