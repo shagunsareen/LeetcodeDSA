ce029de0377efd947f3f4366ceb847018f52541f
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-27 15:05:49 +0530

    Time: 4 ms (71.83%), Space: 44.9 MB (88.25%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -41,7 +41,7 @@ class Solution {
     }*/
 
     //Tabulation - move 1 index forward
-    public int minDistance(String word1, String word2) {
+    /*public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
 
@@ -69,5 +69,36 @@ class Solution {
         }
 
         return dp[n][m];
+    }*/
+
+    //Space optimisation
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] prev = new int[m+1];
+        
+        for(int j = 0; j < m + 1 ; j++){
+            prev[j] = j;
+        }
+
+        for(int i = 1; i <= n; i++){    
+            int[] curr = new int[m+1];
+            curr[0] = i; //as per base case in previous code
+            for(int j = 1; j <= m; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    curr[j] = prev[j-1]; 
+                }else{
+                    int insert = curr[j-1];
+                    int delete = prev[j];
+                    int replace = prev[j-1]; 
+                    curr[j] = 1 + Math.min(insert, Math.min(delete, replace));
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[m];
     }
+
 }
