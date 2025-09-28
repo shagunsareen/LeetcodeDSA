5c7d97933a4fafd7788e511db844a7a8a80750e5
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-28 19:32:06 +0530

    Time: 6 ms (42.39%), Space: 43.2 MB (60.94%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
+    /*public int maxProfit(int k, int[] prices) {
         int n = prices.length;
         int[][] prev = new int[2][k+1];
         int[][] curr = new int[2][k+1];
@@ -23,5 +23,40 @@ class Solution {
             }
         }
         return prev[0][k];
+    }*/
+
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2*k];
+
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+
+        return memoiz(prices, k, 0, n, 0, dp);
     }
+
+    //memoiz
+    private int memoiz(int[] prices, int k, int index, int n, int transNo, int[][] dp){
+        //base case 
+        if(index == n || transNo == 2*k){
+            return 0;
+        }
+
+        if(dp[index][transNo] != -1){
+            return dp[index][transNo];
+        }
+
+        if(transNo % 2 == 0){ //buy 
+            int buy =   -prices[index] + memoiz(prices, k, index+1, n, transNo + 1, dp);
+            int doNothing = memoiz(prices, k, index+1, n, transNo, dp);
+            dp[index][transNo] = Math.max(buy, doNothing);
+        }else{
+            int sell =  prices[index] + memoiz(prices, k, index+1, n, transNo + 1, dp);
+            int doNothing = memoiz(prices, k, index+1, n, transNo, dp);
+            dp[index][transNo] = Math.max(sell, doNothing);
+        }
+
+        return dp[index][transNo];
+    } 
 }
