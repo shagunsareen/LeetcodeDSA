1738b9cea0f1f286acd26a6da0070b826582cda2
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-28 15:49:34 +0530

    Time: 1 ms (77.7%), Space: 46 MB (36.62%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -40,7 +40,7 @@ class Solution {
     }*/
 
     //Tabulation - will go from n-1 to 0 as memoization went from o to n-1 (Bottom up approach)
-    public int maxProfit(int[] prices) {
+    /*public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n+1][2];
 
@@ -65,5 +65,36 @@ class Solution {
         }
 
         return dp[0][0];
+    }*/
+
+    //Space optimisation
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] prev = new int[2];
+
+        //base case if for index == n when we are not left with any more days to buy or sell
+        prev[0] = 0;
+        prev[1] = 0; 
+
+        for(int index = n-1; index >= 0; index--){
+            int[] curr = new int[2];
+
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy == 0){ //we can buy the stock - invest money and go to next index
+                    int buyStock = -prices[index] + prev[1];
+                    int doNothing = prev[0];
+
+                    curr[buy] = Math.max(buyStock , doNothing);
+                }else if(buy == 1){ //sell case 
+                    int sellStock = prices[index] + prev[0];
+                    int doNothing = prev[1];
+
+                    curr[buy] = Math.max(sellStock , doNothing);
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[0];
     }
 }
