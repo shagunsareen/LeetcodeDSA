9609271bb6cdc3e110893b49932424abf036e775
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-28 15:38:11 +0530

    Time: 4 ms (5.54%), Space: 48.7 MB (7.46%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    //Memoization
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2]; //index - buy/notBuy 
+
+        for(int[] row : dp){
+             Arrays.fill(row, -1);
+        }
+
+        return memoiz(prices, dp, 0, 0);
+    }
+
+    //at any point we can store that if we are at a certain index at this point can i buy this stock or not since i can buy only if previous one is sold , this info can be stored in dp array
+    private int memoiz(int[] prices, int[][] dp, int index, int buy){
+        //base case 
+        if(index == prices.length){
+            return 0; //no element left
+        }
+
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
+        }
+
+        if(buy == 0){ //we can buy the stock - invest money and go to next index
+              int buyStock = -prices[index] + memoiz(prices, dp, index + 1, 1);
+              int doNothing = memoiz(prices, dp, index + 1, 0);
+
+              dp[index][buy] = Math.max(buyStock , doNothing);
+        }else if(buy == 1){ //sell case 
+            int sellStock = prices[index] + memoiz(prices, dp, index + 1, 0);
+            int doNothing = memoiz(prices, dp, index + 1, 1);
+
+            dp[index][buy] = Math.max(sellStock , doNothing);
+        }
+
+        return dp[index][buy];
+    }
+}
