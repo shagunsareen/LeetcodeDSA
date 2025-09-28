cefe7b9cf25f7e5cdf888506857316cabd0c7671
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-09-28 20:11:27 +0530

    Time: 1 ms (82.49%), Space: 41.6 MB (91.29%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    //memoiz
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+
+        return memoiz(prices, dp, 0, 0);
+    }
+
+    private int memoiz(int[] prices, int[][] dp, int index, int buy){
+        //base case 
+        if(index >= prices.length){ //since we are jumping to index+2 so if we are at n-1 then we should handle condition with >=
+            return 0;
+        }
+
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
+        }
+
+        if(buy == 0){
+            int buyStock = -prices[index] + memoiz(prices, dp, index + 1, 1);
+            int doNothing = memoiz(prices, dp, index + 1, 0);
+            dp[index][buy] = Math.max(buyStock, doNothing);
+        }else{
+            int sellStock = prices[index] + memoiz(prices, dp, index + 2, 0); //since after selling i cant pick next element so i jump to index + 2
+            int doNothing = memoiz(prices, dp, index + 1, 1);   
+            dp[index][buy] =  Math.max(sellStock, doNothing);
+        }
+
+        return dp[index][buy];
+    }
+}
