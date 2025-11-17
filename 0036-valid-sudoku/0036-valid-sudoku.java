ced830984d5018cd5188ebda56cce6993b75fe86
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-11-17 16:04:44 +0530

    Time: 1 ms (99.76%), Space: 46.4 MB (14.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        /*
+        //approach 1 - tc : O(n2), sc : O(n2) 
         //store values of elements of each row and each col and each block in separate map 
         Map<Integer, Set<Character>> rows = new HashMap<>();
         Map<Integer, Set<Character>> cols = new HashMap<>();
@@ -21,6 +23,44 @@ class Solution {
                 rows.get(row).add(board[row][col]);
                 cols.get(col).add(board[row][col]);
                 cells.get(squareKey).add(board[row][col]);
+            }
+        }
+
+        return true;
+
+        */
+
+        //Approach 2 - Bitmasking, we will check the ith bit in each array to see if the value at current row is set or not i.e. seen or not. 
+        int N = 9;
+        int[] rows = new int[N];
+        int[] cols = new int[N];
+        int[] boxes = new int[N];
+
+        for(int r=0; r<N; r++){
+            for(int c=0; c<N; c++){
+
+                if(board[r][c] == '.') continue;
+
+                int val = board[r][c] - '0';
+                int pos = 1 << (val - 1); //if val was 5 we need to check 4th bit in arr whether its seen or not already
+                
+                if((rows[r] & pos) > 0){ //it will be 1 only if same bit is already 1 
+                    return false;
+                } 
+                rows[r] = rows[r] | pos; //setting bit 
+
+                if((cols[c] & pos) > 0){
+                    return false;
+                }
+
+                cols[c] = cols[c] | pos;
+
+                int boxIndex = (r / 3) * 3 + (c /3); //3 is multiplied to map each box as 0, 1 or 2 
+                if((boxes[boxIndex] & pos) > 0){
+                    return false;
+                }
+
+                boxes[boxIndex] = boxes[boxIndex] | pos;
             }
         }
 
