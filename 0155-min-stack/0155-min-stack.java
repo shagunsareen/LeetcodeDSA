4aab7abc5a7bcb658a5ea49087cef3a1695c9379
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-12-05 15:54:28 +0530

    Time: 5 ms (63.64%), Space: 46.7 MB (52.85%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,5 +1,6 @@
 class MinStack {
 
+    // ******** Approach 1 - One Stack ******** //
     /*private Stack<int[]> stack; 
 
     public MinStack() {
@@ -30,8 +31,8 @@ class MinStack {
         return stack.peek()[1];
     }*/
 
-    //Approach 2 - Two stacks
-    private Stack<Integer> stack; 
+    // ******** Approach 2 - Two stacks ******** //
+    /*private Stack<Integer> stack; 
     private Stack<Integer> minStack;
 
     public MinStack() {
@@ -61,6 +62,45 @@ class MinStack {
     
     public int getMin() {
         return minStack.peek();
+    }*/
+
+    //Approach 3 - Optimised space solution 
+    private Stack<Integer> stack; 
+    private Stack<int[]> minStack; //number and freq of the number 
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty() || val < minStack.peek()[0]){
+            minStack.push(new int[]{val, 1}); //update minstack is this is the smallest value 
+        }else if(val == minStack.peek()[0]){
+            minStack.peek()[1]++;
+        }
+    }
+    
+    public void pop() {
+        //if top of stack is top of minstack pop that as well
+        if(stack.peek().equals(minStack.peek()[0])){
+            minStack.peek()[1]--;
+        }
+
+        if(minStack.peek()[1] == 0){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek()[0];
     }
 }
 
