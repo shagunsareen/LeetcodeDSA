2ab4cc24c9e81f3b727bfa8de9169a31d2aeedba
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-12-06 10:19:41 +0530

    Time: 89 ms (37.4%), Space: 95.6 MB (47.96%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[][] cars = new double[n][2];
+
+        for(int i=0; i<n; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a,b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double lastTime = 0.0;
+
+        for(int i=0; i<n; i++){
+            double time = (target - cars[i][0])/cars[i][1];
+            if(time > lastTime){
+                fleets++;
+                lastTime = time; //slowest time and other cars can see if they complete target in lesser time than this or not 
+            }
+        }
+
+        return fleets;
+    }
+}
