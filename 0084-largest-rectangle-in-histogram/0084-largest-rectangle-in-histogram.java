98ff980a3a5b892f5013f4e3f03893199286bd7c
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-12-06 14:55:00 +0530

    Time: 71 ms (43.58%), Space: 77.7 MB (37.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,76 +1,27 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        
-        //since every height can be in the ans let's take every height as ans height and calculate area
-        //To get area we need prevSmallerIndex and nextSmallerIndex
-        int[] leftSmallerIndexArr = prevSmallerIndex(heights);
-        int[] rightSmallerIndexArr = nextSmallerIndex(heights);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int n = heights.length;
         int maxArea = 0;
-        
-        //now calculate the area
-        for(int i=0; i<heights.length; i++){
-             int leftIndex = leftSmallerIndexArr[i];
-             int rightIndex = rightSmallerIndexArr[i];
-             int height = heights[i];
-            
-             int width = rightIndex - leftIndex - 1;
-             maxArea = Math.max(maxArea, height * width);
+
+        for(int i=0; i<n; i++){    
+            //pop greater elements as we want to keep smaller prev elements in stack 
+            while(stack.peek() != -1 && heights[stack.peek()] > heights[i]){ //rectangle cant be formed with curr smaller height so take stack top as end of rectangle 
+                int currHeight = heights[stack.pop()];
+                int currWidth = i - stack.peek() - 1; // rightBoundary : curr index , leftBoundary : stack top index after popping
+                maxArea = Math.max(maxArea, currHeight * currWidth);
+            }
+            stack.push(i); 
         }
-        
+
+        //if stack has values then calculate area while popping elements
+        while(stack.peek() != -1){
+            int currHeight = heights[stack.pop()];
+            int currWidth = n - stack.peek() - 1; //rightBoundary here is n as we have reached end of array 
+            maxArea = Math.max(maxArea, currHeight * currWidth);
+        }
+
         return maxArea;
-    }
-    
-    
-    private int[] prevSmallerIndex(int[] heights){
-        //we use stack since we need previous elements 
-        Stack<Integer> stack = new Stack<>();
-        int[] leftSmallerIndexArr = new int[heights.length];
-        Arrays.fill(leftSmallerIndexArr, -1);
-        
-        for(int i=0; i<heights.length; i++){
-            
-            //if greater ele is in stack remove it 
-            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
-                //remove greater element on the left
-                stack.pop();
-            }
-            
-            //if we get smaller element in stack than curr height then mark stack ele as smallerLeftIndex
-            if(!stack.isEmpty() && heights[stack.peek()] < heights[i]){
-                leftSmallerIndexArr[i] = stack.peek();
-            }
-            
-            //insert every element since it can be an answer for future elements
-            stack.push(i);
-        }
-        
-        return leftSmallerIndexArr;
-    }
-    
-    
-    private int[] nextSmallerIndex(int[] heights){
-        //we use stack since we need previous elements from the right
-        Stack<Integer> stack = new Stack<>();
-        int[] rightSmallerIndexArr = new int[heights.length];
-        Arrays.fill(rightSmallerIndexArr, heights.length);
-        
-        for(int i=heights.length-1; i>=0; i--){
-            
-            //if greater ele is in stack remove it 
-            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
-                //remove greater element on the left
-                stack.pop();
-            }
-            
-            //if we get smaller element in stack than curr height then mark stack ele as smallerLeftIndex
-            if(!stack.isEmpty() && heights[stack.peek()] < heights[i]){
-                rightSmallerIndexArr[i] = stack.peek();
-            }
-            
-            //insert every element since it can be an answer for future elements
-            stack.push(i);
-        }
-        
-        return rightSmallerIndexArr;
     }
 }
