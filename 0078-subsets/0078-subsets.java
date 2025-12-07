ee60df43b1abcc791da8216ba3d84dce6628678d
shagunsareen <89540572+shagunsareen@users.noreply.github.com>
2025-12-07 20:10:36 +0530

    Time: 1 ms (70.89%), Space: 44.3 MB (7.17%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,29 +1,20 @@
 class Solution {
-    List<List<Integer>> res;
+    private List<List<Integer>> result;
+
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
-        res = new ArrayList<>();
-        ArrayList<Integer> currList = new ArrayList<>();
-        generateSubset(nums, currList, res, 0, n);
-        return res;
+        result = new ArrayList<>();
+        backtrack(0, new ArrayList<>(), nums);
+        return result;
     }
-    
-    
-    private void generateSubset(int[] nums, 
-                                ArrayList<Integer> currList,
-                                List<List<Integer>> res, 
-                                int startIndex, 
-                                int size)
-    {    
-        res.add(new ArrayList<>(currList)); 
+
+    private void backtrack(int index, List<Integer> temp, int[] nums){
+        result.add(new ArrayList<>(temp));
         
-        for(int i=startIndex; i<size; i++){ 
-            //take the current element 
-            currList.add(nums[i]);   
-            //recursively see if you want to take next element or not
-            generateSubset(nums, currList, res, i+1, size);   
-            // do not take the last added element 
-            currList.remove(currList.size() - 1);
+        for(int i = index; i < nums.length; i++){
+            temp.add(nums[i]);
+            backtrack(i+1, temp, nums);
+            temp.remove(temp.size() - 1);
         }
     }
 }
